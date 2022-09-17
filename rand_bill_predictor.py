e9a8d322f3e65eeddba29a44f62416178e8da343
Md. Mamun <mkmamun031@gmail.com>
2022-09-18 02:44:49 +0600

    Fixed list index error!

--- rand_bill_predictor.py
+++ rand_bill_predictor.py
@@ -1,5 +1,6 @@
 import random
+
 billerName = ["Mamun", "Limon", "Shammo", "Faisal", "Shopnil", "Sayed"]
-bill_lenght = len(billerName)
-randNum = random.randint(0 , bill_lenght)
+bill_len = len(billerName)-1
+randNum = random.randint(0, bill_len)
 print(f"{billerName[randNum]} is going to pay the bill today!")
