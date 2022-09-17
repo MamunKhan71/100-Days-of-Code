638d565c7c031e0a4a42484e9b6474000b03ce9a
Md. Mamun <mkmamun031@gmail.com>
2022-09-18 02:50:24 +0600

    Added user input function!

--- rand_bill_predictor.py
+++ rand_bill_predictor.py
@@ -1,6 +1,7 @@
 import random
 
-billerName = ["Mamun", "Limon", "Shammo", "Faisal", "Shopnil", "Sayed"]
-bill_len = len(billerName)-1
+userInput = input("Please add all the names separated by comma(,)\n: ")
+billerName = userInput.split(",")
+bill_len = len(billerName) - 1
 randNum = random.randint(0, bill_len)
 print(f"{billerName[randNum]} is going to pay the bill today!")
