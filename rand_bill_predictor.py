661dba8df714c5de0736e5b261ba5865b7fb786a
Md. Mamun <mkmamun031@gmail.com>
2022-09-18 02:56:07 +0600

    Optimization and Improvements!

--- rand_bill_predictor.py
+++ rand_bill_predictor.py
@@ -2,6 +2,8 @@ import random
 
 userInput = input("Please add all the names separated by comma(,)\n: ")
 billerName = userInput.split(",")
-bill_len = len(billerName) - 1
-randNum = random.randint(0, bill_len)
-print(f"{billerName[randNum]} is going to pay the bill today!")
+# <--! Deprecated --!>
+# bill_len = len(billerName) - 1
+# randNum = random.randint(0, bill_len)
+# <--! Deprecated --!>
+print(f"{random.choice(billerName)} is going to pay the bill today!")
