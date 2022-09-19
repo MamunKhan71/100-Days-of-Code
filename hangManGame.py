6610818f08690dec8ccfd109282cb3b4baf3879c
Md. Mamun <mkmamun031@gmail.com>
2022-09-19 14:36:38 +0600

    Several Bug Fixes! * Fixed Lives deduction didn't work properly. * Added More Word to the game!

--- hangManGame.py
+++ hangManGame.py
@@ -1,25 +1,32 @@
 import random
 
-letter = ['hello', 'banana','error']
+letter = ['rainbow', 'computer', 'science', 'programming',
+          'python', 'mathematics', 'player', 'condition',
+          'reverse', 'water', 'board', 'geeks']
 rand = random.choice(letter)
 randLtr = len(rand)
 blankLtr = ""
 life = 0
+tri = int(0)
+trig = 0
 for num in range(randLtr):
     blankLtr += "_"
 print(f"Guess The Word: {blankLtr}")
 while life != randLtr:
-    userInput = input("Enter Your Letter : ")
+    userInput = input(f"Enter Your Letter (Remaining Lives: {randLtr - life}): ")
+    trig = 0
     for num in range(randLtr):
         if userInput == rand[num]:
             blankLtr = blankLtr[:num] + userInput + blankLtr[num + 1:]
-        if "_" in blankLtr:
-            continue
-    print(blankLtr)
-    if blankLtr == rand:
-        life = randLtr
-    else:
+            trig = 1
+    if "_" not in blankLtr:
+        break
+    elif trig == 0:
         life += 1
+    else:
+        print(blankLtr)
+        continue
+
 if blankLtr == rand:
     print(f"{blankLtr} matches {rand} # You win! - Thanks for saving a life!")
 else:
