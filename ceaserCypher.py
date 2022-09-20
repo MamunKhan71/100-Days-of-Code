a99bd894e7c1617d7c05128aafb076ed7416a748
Md. Mamun <mkmamun031@gmail.com>
2022-09-20 20:13:35 +0600

    Added More Encryption Support and logo for the generator

--- ceaserCypher.py
+++ ceaserCypher.py
@@ -1,3 +1,4 @@
+from art import logo
 letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
           'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
           's', 't', 'u', 'v',
@@ -7,8 +8,13 @@ letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
 def caesar(message, shiftLetter, symbol):
     cypher_text = ""
     new_pos = 0
+    shiftLetter = shiftLetter % 26
     for num in message:
-        pos = letter.index(num)
+        if num not in letter:
+            cypher_text += num
+            continue
+        else:
+            pos = letter.index(num)
         if symbol == "+":
             new_pos = pos + shiftLetter
         elif symbol == "-":
@@ -18,16 +24,21 @@ def caesar(message, shiftLetter, symbol):
     return cypher_text
 
 
-userInput = input("Type encode to encrypt and decode to decrypt a message : ").lower()
+print(logo)
 encrypted_text = ""
 shiftNo = 0
-if userInput == "encode":
-    messageType = input("Type your message here: ").lower()
-    shiftNo = int(input("How many character shift you want? : "))
-    encrypted_text = caesar(shiftLetter=shiftNo, message=messageType, symbol="+")
-    print(f"Your Encrypted Text is : {encrypted_text}")
-elif userInput == "decode":
-    messageType = input("Type your message here: ").lower()
-    shiftNo = int(input("How many character shift you want? : "))
-    decrypted_text = caesar(shiftLetter=shiftNo, message=messageType, symbol="-")
-    print(f"Your Decrypted Text is : {decrypted_text}")
+userChoice = "yes"
+while userChoice == "yes":
+    userInput = input("Type encode to encrypt and decode to decrypt a message : ").lower()
+    if userInput == "encode":
+        messageType = input("Type your message here: ").lower()
+        shiftNo = int(input("How many character shift you want? : "))
+        encrypted_text = caesar(shiftLetter=shiftNo, message=messageType, symbol="+")
+        print(f"Your Encrypted Text is : {encrypted_text}")
+        userChoice = input("Do you want to continue or exit (yes/no) : ")
+    elif userInput == "decode":
+        messageType = input("Type your message here: ").lower()
+        shiftNo = int(input("How many character shift you want? : "))
+        decrypted_text = caesar(shiftLetter=shiftNo, message=messageType, symbol="-")
+        print(f"Your Decrypted Text is : {decrypted_text}")
+        userChoice = input("Do you want to continue or exit (yes/no) : ")
