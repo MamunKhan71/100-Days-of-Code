a743c3880c61e136a4bdd05bc5ffb1e19e9375e4
Md. Mamun <mkmamun031@gmail.com>
2022-09-20 19:39:53 +0600

    * Code Optimization!

--- ceaserCypher.py
+++ ceaserCypher.py
@@ -4,35 +4,30 @@ letter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 
           'w', 'x', 'y', 'z']
 
 
-def encrypt(message, shiftLetter):
+def caesar(message, shiftLetter, symbol):
     cypher_text = ""
+    new_pos = 0
     for num in message:
         pos = letter.index(num)
-        new_pos = pos + shiftLetter
+        if symbol == "+":
+            new_pos = pos + shiftLetter
+        elif symbol == "-":
+            new_pos = pos - shiftLetter
         new_letter = letter[new_pos]
         cypher_text += new_letter
     return cypher_text
 
 
-def decrypt(message, shiftLetter):
-    decypher_text = ""
-    for num in message:
-        pos = letter.index(num)
-        new_pos = pos - shiftLetter
-        new_letter = letter[new_pos]
-        decypher_text += new_letter
-    return decypher_text
-
-userInput = input("Type encode to encrypt and decode to decrypt a message \n:").lower()
+userInput = input("Type encode to encrypt and decode to decrypt a message : ").lower()
 encrypted_text = ""
 shiftNo = 0
 if userInput == "encode":
     messageType = input("Type your message here: ").lower()
     shiftNo = int(input("How many character shift you want? : "))
-    encrypted_text = encrypt(shiftLetter=shiftNo, message=messageType)
+    encrypted_text = caesar(shiftLetter=shiftNo, message=messageType, symbol="+")
     print(f"Your Encrypted Text is : {encrypted_text}")
 elif userInput == "decode":
     messageType = input("Type your message here: ").lower()
     shiftNo = int(input("How many character shift you want? : "))
-    decrypted_text = decrypt(message=messageType, shiftLetter=shiftNo)
+    decrypted_text = caesar(shiftLetter=shiftNo, message=messageType, symbol="-")
     print(f"Your Decrypted Text is : {decrypted_text}")
