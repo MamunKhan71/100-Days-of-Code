8cac8dd9e88a1a7bf304f81f92e6534831e0da1a
Md. Mamun <mkmamun031@gmail.com>
2022-09-21 22:56:46 +0600

    Feature Improvements! * Added Days In Month Counter * Added Leap Year Functionality

---
+++ daysInMonth.py
@@ -0,0 +1,28 @@
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+def days_in_month(years, months):
+    months -= 1
+
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(years):
+        leapDay = month_days[months]
+        return leapDay+1
+    else:
+        return month_days[months]
+
+
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
