035e1e8ba6f448a8321c05efff0f285e7649c25c
Md. Mamun <mkmamun031@gmail.com>
2022-09-22 22:25:14 +0600

    Feature Update: * Several Bug Fixes!

--- blackJack.py
+++ blackJack.py
@@ -9,6 +9,10 @@ def randNumMaker():
     return new_randVal
 
 
+def replace_values(list_to_replace, item_to_replace, item_to_replace_with):
+    return [item_to_replace_with if item == item_to_replace else item for item in list_to_replace]
+
+
 def blackJackCheck(randUsrs):
     i = 0
     while i < 2:
@@ -18,13 +22,41 @@ def blackJackCheck(randUsrs):
             return False
 
 
+def bustCheck(randUsrs, usrSum1):
+    if usrSum1 > 21:
+        if randUsrs.__contains__(11):
+            newVal = randUsrs
+            replace_values(newVal, 11, 1)
+            newSum = sum(newVal)
+            if newSum > 21:
+                return 1
+            else:
+                return 0
+
+
+def userCardChoice(randUsr, userSum):
+    userChoice = input("Do you want to get another Card? (Y/N) : ")
+    if userChoice == "Y":
+        newRand = random.randint(1, 11)
+        randUsr.append(newRand)
+        userSum = sum(randUsr)
+        return userSum
+    else:
+
+
 print(blackJackLogo)
 randUsr = randNumMaker()
-usrSum = randUsr[0] + randUsr[1]
+usrSum = sum(randUsr)
 randCom = randNumMaker()
 comSum = randCom[0] + randCom[1]
 if blackJackCheck(randCom):
     print(f"The Computer Has BlackJack {randCom[0]} and {randCom[1]} : Computer Wins")
 elif blackJackCheck(randUsr):
     print(f"The User Has BlackJack {randUsr[0]} and {randUsr[1]} : User Wins")
-
+else:
+    bustChecker = bustCheck(randUsr, usrSum)
+    if bustChecker == 1:
+        print("You are busted! You Loose!")
+    else:
+        newUserSum = userCardChoice(randUsr, usrSum)
+        bustCheck(randUsr, newUserSum)
