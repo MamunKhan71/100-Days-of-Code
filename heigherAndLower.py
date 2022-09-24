5cd4529da74ae84f1e3b145da760ea876a9d406d
Md. Mamun <mkmamun031@gmail.com>
2022-09-25 01:23:04 +0600

    Optimized Higher and Lower Game!

--- heigherAndLower.py
+++ heigherAndLower.py
@@ -2,6 +2,7 @@ from art import heightWeightLogo, heightWeightLogoVs
 import random
 from dataSet import data
 
+print(heightWeightLogo)
 scoreCount = 0
 loopBreaker = True
 
@@ -12,14 +13,14 @@ while loopBreaker:
     print(heightWeightLogoVs)
     print(f"Against B: {randValue2['name']}, a {randValue2['description']}, from {randValue2['country']}.")
     userChoice = input("Who has more followers? Type 'A' or 'B': ")
-    if userChoice == 'A':
+    if userChoice == 'A' or 'a':
         if randValue1['follower_count'] > randValue2['follower_count']:
             scoreCount += 1
             print(f"You're right! Current score: {scoreCount}.")
         else:
             print(f"Sorry, that's wrong. Final score: {scoreCount}")
             break
-    elif userChoice == 'B':
+    elif userChoice == 'B' or 'b':
         if randValue2['follower_count'] > randValue1['follower_count']:
             scoreCount += 1
             print(f"You're right! Current score: {scoreCount}.")
