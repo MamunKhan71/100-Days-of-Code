4792a66e9f50b3bd92839a3a0c780a8df2c826ee
Md. Mamun <mkmamun031@gmail.com>
2022-09-25 12:35:42 +0600

    Several Updates! * Added Report Generator * Added coinProcessor * Several Bug Fixes!

--- digitalCoffeeMachine.py
+++ digitalCoffeeMachine.py
@@ -2,25 +2,52 @@ import re
 from webbrowser import get
 
 
+def coinProcessor(quate, dim, nickle, pennie):
+    quater = quate * 0.25
+    dimes = dim * 0.10
+    nickles = nickle * 0.05
+    pennies = 0.01
+    total_money = quater + dimes + nickles + pennies
+
+
+def reportGen():
+    global resourcesDirectory
+    firstReD = resourcesDirectory[0]
+    for key, value in firstReD.items():
+        if key != "money":
+            print(f"{key} : {value}")
+        else:
+            print(f"{key} : ${value}")
+
+
 def resources(userCh):
     global resourcesDirectory
+    oldRsDct = resourcesDirectory[0]
     newRsDct = resourcesDirectory[userCh]
-    print(resourcesDirectory[0].get("water") - resourcesDirectory[1].get("water"))
-    for keys,values in resourcesDirectory:
-        
-    
-    
+    # print(resourcesDirectory[0].get("water") - resourcesDirectory[1].get("water"))
+    for keys in newRsDct:
+        if oldRsDct.get(keys) > newRsDct.get(keys):
+            print("Available")
+        else:
+            if newRsDct.get("water") > oldRsDct.get("water"):
+                print("Not Enough Water")
+            if newRsDct.get("milk") > oldRsDct.get("milk"):
+                print("Not Enough milk")
+            if newRsDct.get("coffee") > oldRsDct.get("coffee"):
+                print("Not Enough coffee")
+
+
 userChoice = input("What would you like? (espresso/latte/cappuccino) : ")
 resourcesDirectory = [
     {
         "water": 250,
         "milk": 250,
         "coffee": 100,
-        "money": 0,
+        "money": 10,
     },
     {
-        "water": 100,
-        "milk": 100,
+        "water": 350,
+        "milk": 350,
         "coffee": 30,
         "money": 3.5,
     },
@@ -49,4 +76,4 @@ match userChoice:
         print("Machine is in maintenance mode. Turning off!")
         exit(0)
     case "report":
-        resources(userCh=int(0))
+        reportGen()
