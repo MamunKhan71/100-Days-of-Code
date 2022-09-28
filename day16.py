47cdb7820e6901fe8983f5f6023e636aa4692853
Md. Mamun <mkmamun031@gmail.com>
2022-09-29 01:25:07 +0600

    Added PrettyTable Plugin for better user interface!

---
+++ day16.py
@@ -0,0 +1,5 @@
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Table",["Pikachu", "Squirtle","Charmander"])
+table.add_column("Type",["Electric", "Water","Fire"])
+print(table)
