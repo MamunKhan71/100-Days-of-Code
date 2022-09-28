e240bc6380cb1f968371ba9ef84e44ca03474eb0
Md. Mamun <mkmamun031@gmail.com>
2022-09-29 02:11:54 +0600

    Object-Oriented Programming Implementations! * Added Coffee Maker Machine via OOP! * Easy to understand!

--- day16.py
+++ day16.py
@@ -2,4 +2,5 @@ from prettytable import PrettyTable
 table = PrettyTable()
 table.add_column("Pokemon Table",["Pikachu", "Squirtle","Charmander"])
 table.add_column("Type",["Electric", "Water","Fire"])
+table.align = "l"
 print(table)
