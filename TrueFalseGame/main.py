fe1c3cd256b25007d8dc4f187906ef32442b0927
Md. Mamun <mkmamun031@gmail.com>
2022-09-30 23:58:30 +0600

    Code Optimization! * Score Counter Not Working - #Fixed! * Clean code!

--- TrueFalseGame/main.py
+++ TrueFalseGame/main.py
@@ -9,10 +9,12 @@ print("The quiz will be 5 marks. 1 marks for each")
 print("You need to select true(1) or false(0)")
 questionCount = 0
 correctAnsCount = 0
-while questionCount != 5:
+quizLen = len(questions)
+while questionCount != quizLen:
     question = questions[questionCount]
     actualAns = question["answer"]
-    newModel.questionValidator(question, actualAns)
+    validity = newModel.questionValidator(question, actualAns)
+    if validity:
+        correctAnsCount += 1
     questionCount += 1
 print(f"Test Finished - Your Score is : {correctAnsCount}")
-# newQ = Question(inpt1, inpt2)
--- TrueFalseGame/questionModel.py
+++ TrueFalseGame/questionModel.py
@@ -4,17 +4,8 @@ from database import questions
 class Question:
     def __init__(self):
         pass
-        # self.text = text
-        # self.answer = answer
-        # self.questionAdder()
 
-    # def questionAdder(self):
-    #     newQ = {
-    #         self.text: {self.answer}
-    #     }
-    #     questions.append(newQ)
-
-    def questionValidator(self, question,answer):
+    def questionValidator(self, question, answer):
         print(question["text"])
         choice = input("Your Answer: \n1. True\n2. False\n: ")
         if choice == "1":
@@ -25,3 +16,4 @@ class Question:
             print("Correct Answer!")
         else:
             print("Wrong Answer!")
+            return False
