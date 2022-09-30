bb6add2b3e9d2719d6cd712e654a0e72916afdba
Md. Mamun <mkmamun031@gmail.com>
2022-09-30 23:48:46 +0600

    Code Optimization! * Several Bug Fixes!

--- TrueFalseGame/main.py
+++ TrueFalseGame/main.py
@@ -2,12 +2,17 @@ import random
 from database import questions
 from questionModel import Question
 
+
+newModel = Question()
+random.shuffle(questions)
 print("The quiz will be 5 marks. 1 marks for each")
 print("You need to select true(1) or false(0)")
-
-inpt1 = input("Q: ")
-inpt2 = input("A: ")
-newQ = Question(inpt1, inpt2)
-print("Test")
-
-
+questionCount = 0
+correctAnsCount = 0
+while questionCount != 5:
+    question = questions[questionCount]
+    actualAns = question["answer"]
+    newModel.questionValidator(question, actualAns)
+    questionCount += 1
+print(f"Test Finished - Your Score is : {correctAnsCount}")
+# newQ = Question(inpt1, inpt2)
--- TrueFalseGame/questionModel.py
+++ TrueFalseGame/questionModel.py
@@ -2,13 +2,26 @@ from database import questions
 
 
 class Question:
-    def __init__(self, text, answer):
-        self.text = text
-        self.answer = answer
-        self.questionAdder()
+    def __init__(self):
+        pass
+        # self.text = text
+        # self.answer = answer
+        # self.questionAdder()
 
-    def questionAdder(self):
-        newQ = {
-            self.text: {self.answer}
-        }
-        questions.append(newQ)
+    # def questionAdder(self):
+    #     newQ = {
+    #         self.text: {self.answer}
+    #     }
+    #     questions.append(newQ)
+
+    def questionValidator(self, question,answer):
+        print(question["text"])
+        choice = input("Your Answer: \n1. True\n2. False\n: ")
+        if choice == "1":
+            choice = "True"
+        else:
+            choice = "False"
+        if choice == answer:
+            print("Correct Answer!")
+        else:
+            print("Wrong Answer!")
