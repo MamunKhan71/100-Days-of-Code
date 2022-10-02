9003b4a0ca62997c40bda6f2f49ffd0d8be4a47a
Md. Mamun <mkmamun031@gmail.com>
2022-10-02 21:45:52 +0600

    Code Optimization! * Several Bug fixes!

--- randomWalk/randomWalk.py
+++ randomWalk/randomWalk.py
@@ -2,13 +2,15 @@ import turtle
 import turtle as tim
 import random
 
-colors = ["red", "green", "blue", "yellow", "pink","black"]
+colors = ["red", "green", "blue", "yellow", "pink","black","grey"]
 directions = [0,90,180,270]
 ts = True
 t = tim.Turtle()
+t.speed("fastest")
+t.pensize(10)
 while ts:
-    t.forward(90)
-    t
+    t.forward(30)
+    t.color(random.choice(colors))
     t.seth(random.choice(directions))
 
 tim.exitonclick()
