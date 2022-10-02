6e4f9aa0b665259d78758355949b4363d0a5d879
Md. Mamun <mkmamun031@gmail.com>
2022-10-02 22:06:35 +0600

    Code Optimization! * Added Random Color Picker!

--- randomWalk/randomWalk.py
+++ randomWalk/randomWalk.py
@@ -2,15 +2,27 @@ import turtle
 import turtle as tim
 import random
 
-colors = ["red", "green", "blue", "yellow", "pink","black","grey"]
-directions = [0,90,180,270]
-ts = True
 t = tim.Turtle()
 t.speed("fastest")
+tim.colormode(255)
+
+
+def randomColor():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    colorTpl = (r, g, b)
+    return colorTpl
+
+
+directions = [0, 90, 180, 270]
+ts = True
+
+randomColor()
 t.pensize(10)
 while ts:
     t.forward(30)
-    t.color(random.choice(colors))
+    t.color(randomColor())
     t.seth(random.choice(directions))
 
 tim.exitonclick()
