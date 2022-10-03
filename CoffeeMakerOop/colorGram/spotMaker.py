b566f163996c0a5f6b48d42e08e9cae2ec59a941
Md. Mamun <mkmamun031@gmail.com>
2022-10-03 23:18:41 +0600

    Feature Updates! * Adding Random SpotMaker!

---
+++ CoffeeMakerOop/colorGram/spotMaker.py
@@ -0,0 +1,20 @@
+import colorgram
+import random
+import turtle as trL
+trL.colormode(255)
+colList = []
+colors = colorgram.extract('image.png', 10)
+
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    rgbTuple = (r, g, b)
+    colList.append(rgbTuple)
+
+colLists = [(239, 243, 246), (243, 236, 241), (211, 136, 119), (42, 21, 13), (2, 14, 24), (12, 97, 149), (241, 217, 82),
+            (173, 158, 39)]
+trL.color(random.choice(colLists))
+trL.dot(15)
+
+trL.exitonclick()
