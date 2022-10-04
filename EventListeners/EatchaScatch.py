0f80544e7f51b3b73876b6a89f67c21a0bbb538b
Md. Mamun <mkmamun031@gmail.com>
2022-10-04 10:59:18 +0600

    Added new game Eatch a Scatch! * Added Move Forward Function * Added Move Backward Function * Added Move Clock-Wise Function * Added Move Counter-clock-wise Function * Added Screen Clearing Function

--- EventListeners/EatchaScatch.py
+++ EventListeners/EatchaScatch.py
@@ -2,6 +2,8 @@ from turtle import Turtle, Screen
 
 jenny = Turtle()
 screen = Screen()
+headings = 0
+jenny.pensize(3)
 
 
 def move_forward():
@@ -12,13 +14,38 @@ def move_backward():
     jenny.backward(10)
 
 
-# def move_clockwise():
-#
-# def move_counterClock():
+def move_clockwise():
+    global headings
+    headings += 10
+    jenny.seth(headings)
+    if headings == 360:
+        headings = 0
+
+
+def clearScreen():
+    global headings
+    jenny.clear()
+    headings = 0
+    jenny.seth(headings)
+    jenny.penup()
+    jenny.setx(0.00)
+    jenny.sety(0.00)
+    jenny.pendown()
+
+
+def move_counterClock():
+    global headings
+    headings -= 10
+    jenny.seth(headings)
+
+    if headings == -360:
+        headings = 0
 
 
 screen.listen()
 screen.onkey(key="w", fun=move_forward)
 screen.onkey(key="s", fun=move_backward)
-screen.onkey(key="a", fun=move_backward)
+screen.onkey(key="a", fun=move_clockwise)
+screen.onkey(key="d", fun=move_counterClock)
+screen.onkey(key="c", fun=clearScreen)
 screen.exitonclick()
