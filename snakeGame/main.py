c83f3891cd57a2599d8a6b78de1479c1219522fb
Md. Mamun <mkmamun031@gmail.com>
2022-10-06 22:00:04 +0600

    Snake game modification!

--- snakeGame/main.py
+++ snakeGame/main.py
@@ -1,9 +1,21 @@
 import pyautogui
 from turtle import Turtle,Screen
 screen = Screen()
-tim = Turtle("square")
-tim.color("white")
-screen.setup(height=600,width=600)
+turtles = []
+startPos = [(0,0),(-20,0),(-40,0)]
+for pos in startPos:
+    newTurtles = Turtle("square")
+    newTurtles.color("white")
+    newTurtles.goto(pos)
+    turtles.append(newTurtles)
+gameisOn = True
+while gameisOn:
+    for ttl in turtles:
+        ttl.forward(20)
+
+screen.setup(height=600, width=600)
 screen.bgcolor('black')
+screen.title("Snake Game")
 screen.exitonclick()
+
 
