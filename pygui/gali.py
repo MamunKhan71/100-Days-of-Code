5e94d291f193714a4be7e9680e84f41e40bfcd95
Md. Mamun <mkmamun031@gmail.com>
2022-10-07 21:27:17 +0600

    Several Code Improvements!

--- pygui/gali.py
+++ pygui/gali.py
@@ -1,12 +1,13 @@
-import pyautogui,pyperclip
+import pyautogui, pyperclip
 import time
 
-txt = open('animals.txt','r')
-a = "You are a"
+txt = open('propose.txt', 'r')
+a = input("Enter your Name: ")
 for i in txt:
     pyautogui.write(a+' '+i)
     pyautogui.press('Enter')
     time.sleep(.3)
+
 
 
 
