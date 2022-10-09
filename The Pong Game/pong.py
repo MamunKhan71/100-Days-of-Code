6428ec02dd56b7b5c99d7eaae49f71d76a55279d
Md. Mamun <mkmamun031@gmail.com>
2022-10-09 19:33:55 +0600

    Feature Updates! * Several Updates of The new Pong game!

--- The Pong Game/pong.py
+++ The Pong Game/pong.py
@@ -1,15 +1,26 @@
 from turtle import Turtle, Screen
 
+screen = Screen()
+screen.tracer(0)
+
 
 class Pong:
     def __init__(self):
-        self.createPong()
+        self.newBar = []
+        self.newPong = Turtle("square")
+        self.newPong.color("white")
+        self.newPong.goto(0.0, 0.0)
+        self.createBar()
 
-    def createPong(self):
-        newPong = Turtle("square")
-        newPong.color("white")
-        newPong.goto(0.0, 0.0)
     def createBar(self):
-        for num in range(3):
-            newBar = Turtle()
-            self.
+        x_pos = -380
+        y_pos = -20
+        for num in range(4):
+            newBar = Turtle("square")
+            newBar.color("white")
+            newBar.penup()
+            newBar.setx(x_pos)
+            newBar.sety(y_pos)
+            self.newBar.append(newBar)
+            y_pos += 20
+        screen.update()
