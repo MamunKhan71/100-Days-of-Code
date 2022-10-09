fe8bf3e8adbc31e8621133abbedafa1c23e39c96
Md. Mamun <mkmamun031@gmail.com>
2022-10-09 19:08:10 +0600

    General Code updates!

--- The Pong Game/pong.py
+++ The Pong Game/pong.py
@@ -1,15 +1,19 @@
 from turtle import Turtle, Screen
 
+x_pos = 0
+y_pos = 0
+
 
 class Pong:
     def __init__(self):
-        self.createPong()
+        self.netBar = []
+        self.newPong = Turtle("square")
+        self.newPong.color("white")
+        self.newPong.goto(0.0, 0.0)
+        self.createBar()
 
-    def createPong(self):
-        newPong = Turtle("square")
-        newPong.color("white")
-        newPong.goto(0.0, 0.0)
     def createBar(self):
         for num in range(3):
-            newBar = Turtle()
-            self.
+            newBar = Turtle("square")
+            newBar.color("white")
+            newBar.goto()
