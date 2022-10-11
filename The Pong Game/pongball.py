bb0a1116f76746e70b153ae82a99de3f1c4c75a6
Md. Mamun <mkmamun031@gmail.com>
2022-10-11 22:18:40 +0600

    Feature Updates! * Added Ball bouncing Mechanism!

--- The Pong Game/pongball.py
+++ The Pong Game/pongball.py
@@ -3,17 +3,20 @@ from turtle import Turtle
 
 class PongBall:
     def __init__(self):
+        self.x_cor = 10
+        self.y_cor = 10
         self.newPong = Turtle("circle")
         self.newPong.color("white")
         self.newPong.goto(0.0, 0.0)
         self.newPong.penup()
 
     def ballMover(self):
-        new_x = self.newPong.xcor() + 10
-        new_y = self.newPong.ycor() + 10
+        new_x = self.newPong.xcor() + self.x_cor
+        new_y = self.newPong.ycor() + self.y_cor
         self.newPong.goto(new_x, new_y)
-        if self.newPong.ycor() == 280:
-            print("inside")
-            new_x = self.newPong.xcor() - 10
-            new_y = self.newPong.ycor() - 10
-            self.newPong.goto(new_x, new_y)
+        if self.newPong.ycor() > 280 or self.newPong.ycor() < -280 :
+            self.bounce()
+
+    def bounce(self):
+        self.y_cor *= -1
+
