3d088738061c97558fbb2404507eb74d02257b0e
Md. Mamun <mkmamun031@gmail.com>
2022-10-13 10:47:12 +0600

    Road Crossing Game Updates! * Several Code updates to the car manager class * Generating random cars at random position! #Working!

--- Road Crossing Game/car_manager.py
+++ Road Crossing Game/car_manager.py
@@ -1,7 +1,23 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+    def __init__(self):
+        self.carList = []
+        self.setX = 300
+        self.carCreator()
+
+    def carCreator(self):
+        newCar = Turtle("square")
+        newCar.seth(180)
+        for _ in range(6):
+            setY = random.randint(-250, 250)
+            newCar.color(random.choice(COLORS))
+            newCar.shapesize(stretch_wid=2, stretch_len=1)
+            newCar.goto(self.setX, setY)
+            self.carList.append(newCar)
+
