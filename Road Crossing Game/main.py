4d946e187fee41c6b43326b4bb2ed3aad6b877f3
Md. Mamun <mkmamun031@gmail.com>
2022-10-13 10:25:20 +0600

    Road Crossing Game Updates! * Added Player * Player is moving on pressing Up key on keyboard!

--- Road Crossing Game/main.py
+++ Road Crossing Game/main.py
@@ -7,8 +7,10 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
-
+screen.listen()
+player = Player()
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
+    screen.onkeypress(fun=player.playerUp, key="Up")
     screen.update()
--- Road Crossing Game/player.py
+++ Road Crossing Game/player.py
@@ -1,7 +1,18 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("black")
+        self.penup()
+        self.left(90)
+        self.goto(STARTING_POSITION)
+
+    def playerUp(self):
+        self.forward(10)
