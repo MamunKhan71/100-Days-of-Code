68daed1ad143f0dca73978d8540328ea47db6b5b
Md. Mamun <mkmamun031@gmail.com>
2022-10-15 00:46:33 +0600

    Feature Updates! * Mail Merging Code Updates! - Fixes Coming Soon!

--- Mail Merging/main.py
+++ Mail Merging/main.py
@@ -1,1 +1,15 @@
 # Adding Mail Merging with customization features!
+with open("./Input/Name/names.txt", "r") as names:
+    with open("./Input/Letter/letter.txt", "r") as letter:
+        for num in names:
+            newNum = num.strip("\n")
+            for ltr in letter:
+                with open(f"./Output/ReadyToSend/{newNum}.txt", "w") as modFile:
+                    newMail = ltr.replace("[name]", newNum)
+                    # modFile.write(newMail)
+
+                    modFile.write(str(newMail))
+
+
+
+
