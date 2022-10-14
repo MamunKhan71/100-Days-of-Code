3a6a9a3a48d2f714058a510c32f7e3369f32eaf9
Md. Mamun <mkmamun031@gmail.com>
2022-10-15 01:21:19 +0600

    Feature Updates! * Mail Merging Code Updates! - Automatic Mail Generator Working Fine!

--- Mail Merging/main.py
+++ Mail Merging/main.py
@@ -1,14 +1,13 @@
 # Adding Mail Merging with customization features!
 with open("./Input/Name/names.txt", "r") as names:
     with open("./Input/Letter/letter.txt", "r") as letter:
+        ltr = letter.read()
         for num in names:
             newNum = num.strip("\n")
-            for ltr in letter:
-                with open(f"./Output/ReadyToSend/{newNum}.txt", "w") as modFile:
-                    newMail = ltr.replace("[name]", newNum)
-                    # modFile.write(newMail)
+            with open(f"./Output/ReadyToSend/{newNum}.txt", "w") as modFile:
+                newNumMail = ltr.replace("[name],", num)
+                modFile.write(newNumMail)
 
-                    modFile.write(str(newMail))
 
 
 
