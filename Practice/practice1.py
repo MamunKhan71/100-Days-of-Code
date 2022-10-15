b7b481d69390dea986c86588ae3f1f49d6f21e6f
Md. Mamun <mkmamun031@gmail.com>
2022-10-15 14:07:11 +0600

    Feature Updates! * Opted Pandas library to work more efficiently!

--- Practice/practice1.py
+++ Practice/practice1.py
@@ -1,8 +1,13 @@
 import csv
-with open("weather_report.csv", "r") as weather:
-    data = csv.reader(weather)
-    temperatures = []
-    for row in data:
-        if row[1] != "temp":
-            temperatures.append(row[1])
-    print(temperatures)
+import pandas as pd
+
+# with open("weather_report.csv", "r") as weather:
+#     data = csv.reader(weather)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#
+#     print(temperatures)
+weatherData = pd.read_csv("weather_report.csv")
+print(weatherData["temp"])
