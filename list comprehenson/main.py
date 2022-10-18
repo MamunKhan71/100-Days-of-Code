fc4f897370359cea572489596b197fcf438b345d
Md. Mamun <mkmamun031@gmail.com>
2022-10-18 21:28:19 +0600

    ## Working with List Comprehension ! * Reading File and doing several operations!

--- list comprehenson/main.py
+++ list comprehenson/main.py
@@ -1,6 +1,9 @@
 file1 = open("file1.txt")
 file2 = open("file2.txt")
 
-newlist = [num for num in file1]
+newfile1 = file1.readlines()
+newfile2 = file2.readlines()
 
-print(newlist)
+newList = [lists.strip() for lists in newfile1 if newfile2.__contains__(lists)]
+aFile = newfile2
+print(newList)
