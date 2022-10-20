4069102444ee6fadc75243929d3ff90a4ae36a5f
Md. Mamun <mkmamun031@gmail.com>
2022-10-21 01:24:25 +0600

    ## Feature Updates! -> Working with Graphical User Interface!

---
+++ Practice/main.py
@@ -0,0 +1,11 @@
+import tkinter
+window = tkinter.Tk()
+window.title("My First Program - GUI")
+window.minsize(width=500,height=300)
+new_label = tkinter.Label(text="\n\n\n\n\n\n\n\nAll I wanted to say is\nI Love You! Zarin!", font=("Consolas",10,"bold"))
+new_label.pack()
+
+
+
+
+window.mainloop()
