472b0af712ef41143a267fbfdd1555cfa0d6974a
Md. Mamun <mkmamun031@gmail.com>
2022-10-22 22:25:43 +0600

    ## Feature Updates! -> Code updates! -> Working with GUI! Tkinter!

--- Practice/main.py
+++ Practice/main.py
@@ -20,3 +20,34 @@ def action():
 button = Button(text="Click Me", command=action)
 button.pack()
 
+# Entries
+entry = Entry(width=30)
+# Add some text to begin with
+entry.insert(END, string="Some text to begin with.")
+# Gets text in entry
+print(entry.get())
+entry.pack()
+
+# Text
+text = Text(height=5, width=30)
+# Puts cursor in textbox.
+text.focus()
+# Adds some text to begin with.
+text.insert(END, "Example of multi-line text entry.")
+# Get's current value in textbox at line 1, character 0
+print(text.get("1.0", END))
+text.pack()
+
+
+# Spinbox
+def spinbox_used():
+    # gets the current value in spinbox.
+    print(spinbox.get())
+
+
+spinbox = Spinbox(from_=0, to=10, width=5, command=spinbox_used)
+spinbox.pack()
+
+
+# Scale
+# Called with current scale value.
