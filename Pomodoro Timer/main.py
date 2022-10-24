294022a380787848f128f76ae24042a6c465b00b
Md. Mamun <mkmamun031@gmail.com>
2022-10-24 23:58:34 +0600

    ## Feature Updates! -> User Interface Updates!

--- Pomodoro Timer/main.py
+++ Pomodoro Timer/main.py
@@ -14,15 +14,16 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title("Tomato Timer")
-window.config(pady=50, padx=50, bg=bg)
-canvas = Canvas(width=500, height=500, bg=bg, highlightthickness=0)
-canvas.create_text(250, 30, text="Pomodoro Timer", font=("Consolas", 25, "bold"))
+window.config(bg=bg, width=500, height=500, padx=100, pady=50)
+canvas = Canvas(width=300, height=400, bg=bg, highlightthickness=0)
+canvas.create_text(150, 30, text="Pomodoro Timer", font=("Consolas", 25, "bold"))
 tomatoImg = PhotoImage(file="tomatoTimerPng.png")
-canvas.create_image(250, 200, image=tomatoImg)
-canvas.create_text(250, 370, text="00:00", font=("Consolas", 40, "bold"))
-canvas.pack()
-
-
-
+canvas.create_image(150, 200, image=tomatoImg)
+canvas.create_text(150, 370, text="00:00", fill="red", font=("Consolas", 40, "bold"))
+start = Button(text="Start", width=10, bg="black", fg="white", highlightthickness=0)
+reset = Button(text="Reset", width=10, bg="blue", fg="white", highlightthickness=0)
+start.grid(row=2, column=0)
+reset.grid(row=2, column=2)
+canvas.grid(row=1, column=1)
 
 window.mainloop()
