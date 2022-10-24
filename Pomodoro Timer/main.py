81e5d7d21e7892b65e0a8bbb5a7344c2144d896a
Md. Mamun <mkmamun031@gmail.com>
2022-10-24 22:38:07 +0600

    ## Feature Updates! -> Adding Pomodoro Timer -> User Interface Updates!

--- Pomodoro Timer/main.py
+++ Pomodoro Timer/main.py
@@ -6,6 +6,7 @@ PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
+bg = "#F9F9F9"
 FONT_NAME = "Courier"
 WORK_MIN = 1
 SHORT_BREAK_MIN = 5
@@ -13,11 +14,15 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title("Tomato Timer")
-window.config(pady=50, padx=50)
-canvas = Canvas(width=500, height=500)
-canvas.create_text(250, 30, text="Pomodoro Timer", font=("Consolas", 24, "bold"))
+window.config(pady=50, padx=50, bg=bg)
+canvas = Canvas(width=500, height=500, bg=bg, highlightthickness=0)
+canvas.create_text(250, 30, text="Pomodoro Timer", font=("Consolas", 25, "bold"))
 tomatoImg = PhotoImage(file="tomatoTimerPng.png")
 canvas.create_image(250, 200, image=tomatoImg)
+canvas.create_text(250, 370, text="00:00", font=("Consolas", 40, "bold"))
 canvas.pack()
+
+
+
 
 window.mainloop()
