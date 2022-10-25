22b3d3d24eb36d6ca3b751a5453d98f6f8720bcc
Md. Mamun <mkmamun031@gmail.com>
2022-10-25 09:50:13 +0600

    ## Feature Updates! -> User Interface Updates! -> Added Check Mark for counting the pomodoro!

--- Pomodoro Timer/main.py
+++ Pomodoro Timer/main.py
@@ -13,17 +13,17 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
 window = Tk()
-window.title("Tomato Timer")
+window.title("Pomodoro Timer")
 window.config(bg=bg, width=500, height=500, padx=100, pady=50)
 canvas = Canvas(width=300, height=400, bg=bg, highlightthickness=0)
-canvas.create_text(150, 30, text="Pomodoro Timer", font=(FONT_NAME, 25, "bold"))
+canvas.create_text(150, 20, text="Pomodoro Timer", font=(FONT_NAME, 25, "bold"))
 tomatoImg = PhotoImage(file="tomatoTimerPng.png")
-canvas.create_image(150, 200, image=tomatoImg)
-canvas.create_text(150, 370, text="00:00", fill="red", font=(FONT_NAME, 40, "bold"))
+canvas.create_image(150, 180, image=tomatoImg)
+canvas.create_text(150, 350, text="00:00", fill="red", font=(FONT_NAME, 40, "bold"))
 start = Button(text="Start", width=10, bg="black", fg="white", highlightthickness=0)
 reset = Button(text="Reset", width=10, bg="blue", fg="white", highlightthickness=0)
-
-
+checkMark = Label(text="✅", borderwidth=2, relief="flat")
+checkMark.grid(row=2, column=1)
 start.grid(row=2, column=0)
 reset.grid(row=2, column=2)
 canvas.grid(row=1, column=1)
