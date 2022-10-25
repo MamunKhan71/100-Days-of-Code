60ff7d24ad1173bafc194a56555d048e66dfad84
Md. Mamun <mkmamun031@gmail.com>
2022-10-25 14:27:00 +0600

    ## Pomodoro User Interface Updates -> Fixed an element showing unnecessarily! -> Removed Unnecessary variable to speed up the program -> Added pomodoro technic timer -> Several Code Improvements -> Added Comment for better readability

--- Pomodoro Timer/main.py
+++ Pomodoro Timer/main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import math
 
-# ---------------------------- CONSTANTS ------------------------------- #
+# ---------------------------- PROGRAM CONSTANTS ------------------------------- #
 bg = "#F9F9F9"
 FONT_NAME = "Consolas"
 WORK_MIN = 25
@@ -11,7 +11,7 @@ reps = 0
 timer = None
 
 
-# ---------------------------- TIMER ------------------------------- #
+# ---------------------------- TIMER RESET ------------------------------- #
 def timeReset():
     global reps
     reps = 0
@@ -20,6 +20,8 @@ def timeReset():
     canvas.itemconfig(timeCounters, text=f"00:00")
     window.after_cancel(timer)
 
+
+# ---------------------------- TIMER COUNT ------------------------------- #
 
 def timeCounter(count):
     global reps, timer
@@ -42,6 +44,8 @@ def timeCounter(count):
         checkMark.config(text=mark, bg="green", fg="white")
 
 
+# ---------------------------- START TIMER ------------------------------- #
+
 def startTimer():
     global reps
     reps += 1
@@ -60,21 +64,45 @@ def startTimer():
         canvas.itemconfig(titleText, text=f"Work", fill="green")
 
 
+# ---------------------------- MAIN FUNCTION ------------------------------- #
+
+# Initializing Window
 window = Tk()
 window.title("Pomodoro Timer")
 window.config(bg=bg, width=500, height=500, padx=100, pady=50)
+
+# Creating Canvas
+
 canvas = Canvas(width=300, height=430, bg=bg, highlightthickness=0)
+# Default Text for the app
+
 titleText = canvas.create_text(150, 20, text="Pomodoro Timer", font=(FONT_NAME, 25, "bold"))
+
+# Image for the app
+
 tomatoImg = PhotoImage(file="tomatoTimerPng.png")
 canvas.create_image(150, 180, image=tomatoImg)
+
+# Countdown Timer
+
 timeCounters = canvas.create_text(150, 350, text="00:00", fill="red", font=(FONT_NAME, 40, "bold"))
+
+# Start and Reset Button Functionality
 start = Button(text="Start", width=10, bg="black", fg="white", highlightthickness=0, command=startTimer)
 reset = Button(text="Reset", width=10, bg="blue", fg="white", highlightthickness=0, command=timeReset)
+
+# Bar for task completion checkmark
+
 barImg = PhotoImage(file="complete.png")
 barImage = canvas.create_image(150, 400, image=barImg)
 checkMark = Label(borderwidth=2, relief="flat", highlightthickness=0)
+
+# Moving to position using grid method
+
 checkMark.grid(row=2, column=1)
 start.grid(row=2, column=0)
 reset.grid(row=2, column=2)
 canvas.grid(row=1, column=1)
+
+# Mainloop here
 window.mainloop()
