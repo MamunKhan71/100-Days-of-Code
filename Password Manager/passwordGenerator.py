48aa37f118c86c708bac487c25eefd5b24e0ca5b
Md. Mamun <mkmamun031@gmail.com>
2022-10-26 22:11:36 +0600

    ## New Features! -> Added Password Generator -> Functionality will be added later!

--- Password Manager/passwordGenerator.py
+++ Password Manager/passwordGenerator.py
@@ -2,25 +2,24 @@ import random
 
 
 class passGen:
-    passWSavers = []
 
     def __init__(self):
-        self.lTrs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-                     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-        self.numBers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-        self.symBols = ['!', '#', '$', '%', '&', ',', '*', '+', '@']
+        passWSavers = []
+        lTrs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+        numBers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        symBols = ['!', '#', '$', '%', '&', ',', '*', '+', '@']
 
 
-def passWGen(self):
+def passWGen():
     print("Welcome to PyPassword Generator")
     letterCount = int(15)
     numberCount = random.randint(1, 5)
     symbolCount = random.randint(1, 5)
-
     for num in range(0, letterCount):
-        rand_char = random.choice(self.lTrs)
+        rand_char = random.choice(numBers)
         self.passWSavers.append(rand_char)
     for num in range(0, numberCount):
         rand_num = random.choice(self.numBers)
@@ -28,5 +27,10 @@ def passWGen(self):
     for num in range(0, symbolCount):
         rand_sym = random.choice(self.symBols)
         self.passWSavers.append(rand_sym)
+    passViewer(self)
 
 
+def passViewer(self):
+    random.shuffle(self.passWSavers)
+    genString = ''.join(self.passWSavers)
+    print(f"Randomly Generated Pass is : {genString}")
