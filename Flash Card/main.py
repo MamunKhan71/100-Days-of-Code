218e973b89f167c270f3588a7dd8a4e388cd746d
Md. Mamun <mkmamun031@gmail.com>
2022-10-30 00:33:07 +0600

    Flesh Card Game Updates! * UI Updates!

--- Flash Card/main.py
+++ Flash Card/main.py
@@ -1,12 +1,14 @@
 from tkinter import *
-BACKGROUND_COLOR = "5D1049"
+BACKGROUND_COLOR = "#5D1049"
 
 window = Tk()
-window.minsize(height=500, width=600)
 window.title("Flashy")
-window.config(pady=30, padx=30)
+window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
 
-canvas = Canvas(height=500, width=600)
-cardImage = PhotoImage(file="./images/")
-canvas.create_image()
+canvas = Canvas(height=526, width=771, highlightthickness=0)
+cardImage = PhotoImage(file="./images/Flash1.png")
+canvas.create_image(384, 260, image=cardImage)
+canvas.config(bg=BACKGROUND_COLOR)
+canvas.create_text(192, 130, text="Hello There")
+canvas.grid(row=0, column=0)
 window.mainloop()
