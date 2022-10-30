773a5e53b864b072e66e13d821bea4a6e270f68e
Md. Mamun <mkmamun031@gmail.com>
2022-10-30 13:34:55 +0600

    Flesh Card Game Updates! * Added English to French Conversion * Button is now working fine.

--- Flash Card/main.py
+++ Flash Card/main.py
@@ -7,11 +7,22 @@ database = pd.read_csv("./data/french_words.csv")
 dbList = database.to_dict(orient="records")
 print(dbList)
 
+current_card = {}
+
 
 def nextCardChooser():
+    global current_card
     current_card = random.choice(dbList)
     canvas.itemconfig(titleText, text=f"French")
     canvas.itemconfig(questionText, text=f"{current_card['French']}")
+
+
+def cardFlipper():
+    global current_card
+    # newImage = PhotoImage(file="./images/ffb.png")
+    # canvas.itemconfig(canvasImage, image=newImage)
+    canvas.itemconfig(titleText, text="English")
+    canvas.itemconfig(questionText, text=current_card["English"])
 
 
 window = Tk()
@@ -20,7 +31,7 @@ window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(height=526, width=771, highlightthickness=0)
 cardImage = PhotoImage(file="./images/ff.png")
-canvas.create_image(384, 260, image=cardImage)
+canvasImage = canvas.create_image(384, 260, image=cardImage)
 canvas.config(bg=BACKGROUND_COLOR)
 titleText = canvas.create_text(384, 180, font=("Born Rounded Demo", 24, "italic underline"))
 questionText = canvas.create_text(384, 263, font=("Born Rounded Demo", 24, "bold"))
@@ -29,7 +40,7 @@ rightImg = PhotoImage(file="./images/buttonr.png")
 rightBtn = Button(image=rightImg, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0, command=nextCardChooser)
 rightBtn.grid(row=1, column=0)
 leftImg = PhotoImage(file="./images/buttonx.png")
-leftBtn = Button(image=leftImg, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0, command=nextCardChooser)
+leftBtn = Button(image=leftImg, highlightthickness=0, bg=BACKGROUND_COLOR, borderwidth=0, command=cardFlipper)
 leftBtn.grid(row=1, column=1)
 nextCardChooser()
 window.mainloop()
