095e91e95a72e45de6b615fb6f2525b7f197fc42
Md. Mamun <mkmamun031@gmail.com>
2022-10-30 14:07:24 +0600

    Flesh Card Game Updates! # Bug Fixes * Fixed a bug that was causing the flip timer not to count for 3 seconds properly.

--- Flash Card/main.py
+++ Flash Card/main.py
@@ -3,25 +3,26 @@ from tkinter import *
 import pandas as pd
 
 BACKGROUND_COLOR = "#563F6D"
+NEW_BACKGROUND_COLOR = "#56AB45"
 database = pd.read_csv("./data/french_words.csv")
 dbList = database.to_dict(orient="records")
-print(dbList)
 
 current_card = {}
 
 
 def nextCardChooser():
-    global current_card
+    global current_card, flipWindow
+    window.after_cancel(flipWindow)
     current_card = random.choice(dbList)
+    canvas.itemconfig(canvasImage, image=cardImage)
     canvas.itemconfig(titleText, text=f"French")
     canvas.itemconfig(questionText, text=f"{current_card['French']}")
-    canvas.after(3000, func=cardFlipper)
+    flipWindow = window.after(3000, func=cardFlipper)
 
 
 def cardFlipper():
     global current_card
-    # newImage = PhotoImage(file="./images/ffb.png")
-    # canvas.item-config(canvasImage, image=newImage)
+    canvas.itemconfig(canvasImage, image=backImg)
     canvas.itemconfig(titleText, text="English")
     canvas.itemconfig(questionText, text=current_card["English"])
 
@@ -29,9 +30,10 @@ def cardFlipper():
 window = Tk()
 window.title("Flashy")
 window.config(pady=50, padx=50, bg=BACKGROUND_COLOR)
-
+flipWindow = window.after(3000, func=nextCardChooser)
 canvas = Canvas(height=526, width=771, highlightthickness=0)
 cardImage = PhotoImage(file="./images/ff.png")
+backImg = PhotoImage(file="./images/ffb.png")
 canvasImage = canvas.create_image(384, 260, image=cardImage)
 canvas.config(bg=BACKGROUND_COLOR)
 titleText = canvas.create_text(384, 180, font=("Born Rounded Demo", 24, "italic underline"))
