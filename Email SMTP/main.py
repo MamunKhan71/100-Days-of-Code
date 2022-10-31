ceb1cedaaa2fca7c0142361e9fa9db8c6f2e4b16
Md. Mamun <mkmamun031@gmail.com>
2022-10-31 12:17:32 +0600

    Email SMTP * SMTP is Configured, But not working. * Update will be merged soon.

--- Email SMTP/main.py
+++ Email SMTP/main.py
@@ -1,5 +1,24 @@
 import smtplib
-myEmail = "mamunkhan3523@gmail.com"
+import ssl
+server = None
+smtp_server = "smtp.gmail.com"
+port = 587
+sender_email = "mamunkhan3523@gmail.com"
+password = "AtgsWbics3523"
+myMsg = "Congratulations! Your mail is received successfully!"
+context = ssl.create_default_context()
+try:
+    server = smtplib.SMTP(smtp_server, port)
+    server.ehlo()
+    server.starttls(context=context)
+    server.ehlo()
+    server.login(sender_email, password)
+    server.sendmail(from_addr=sender_email, to_addrs=password, msg=myMsg)
+except Exception as e:
+    print("Message sending fail. Please Try Again!")
+    print(e)
 
-connect = smtplib.SMTP("smtp.gmail.com")
-connect.starttls()
+finally:
+    server.quit()
+
+
