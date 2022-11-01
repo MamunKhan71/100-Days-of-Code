246f8fe4fe9293714e46bede3762b0b99daa7bdc
Md. Mamun <mkmamun031@gmail.com>
2022-11-01 10:09:13 +0600

    Email SMTP * SMTP Sending mail is working just fine ##Fixed.

--- Email SMTP/main.py
+++ Email SMTP/main.py
@@ -1,24 +1,11 @@
 import smtplib
-import ssl
-server = None
-smtp_server = "smtp.gmail.com"
-port = 587
-sender_email = "mamunkhan3523@gmail.com"
-password = "AtgsWbics3523"
-myMsg = "Congratulations! Your mail is received successfully!"
-context = ssl.create_default_context()
-try:
-    server = smtplib.SMTP(smtp_server, port)
-    server.ehlo()
-    server.starttls(context=context)
-    server.ehlo()
-    server.login(sender_email, password)
-    server.sendmail(from_addr=sender_email, to_addrs=password, msg=myMsg)
-except Exception as e:
-    print("Message sending fail. Please Try Again!")
-    print(e)
-
-finally:
-    server.quit()
-
-
+myEmail = "mail@gmail.com"
+myPass = "pass"
+myMsg = "Subject:Hello Test\n\nHi there this is an automated email. Testing the workability."
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=myEmail, password=myPass)
+    connection.sendmail(from_addr=myEmail,
+                        to_addrs="mamunkhan3523@yahoo.com",
+                        msg=myMsg
+    )
