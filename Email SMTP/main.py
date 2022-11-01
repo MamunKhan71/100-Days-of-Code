133be7361cdf31e280713cbad5afe37c726cd61d
Md. Mamun <mkmamun031@gmail.com>
2022-11-01 10:21:14 +0600

    #Feature Updates * Working with email SMTP to send random motivational quotes in main every day.

--- Email SMTP/main.py
+++ Email SMTP/main.py
@@ -1,11 +1,14 @@
-import smtplib
-myEmail = "mail@gmail.com"
-myPass = "pass"
-myMsg = "Subject:Hello Test\n\nHi there this is an automated email. Testing the workability."
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=myEmail, password=myPass)
-    connection.sendmail(from_addr=myEmail,
-                        to_addrs="mamunkhan3523@yahoo.com",
-                        msg=myMsg
-    )
+# import smtplib
+# myEmail = "mail@gmail.com"
+# myPass = "pass"
+# myMsg = "Subject:Hello Test\n\nHi there this is an automated email. Testing the workability."
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=myEmail, password=myPass)
+#     connection.sendmail(from_addr=myEmail,
+#                         to_addrs="mamunkhan3523@yahoo.com",
+#                         msg=myMsg
+#     )
+import datetime as dt
+myBirth = dt.datetime(year=1999, month=7,day=1)
+print(myBirth)
