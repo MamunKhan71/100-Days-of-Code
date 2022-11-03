8f6b647f63ba0bc22b57112bcee7c4f208baf1cf
Md. Mamun <mkmamun031@gmail.com>
2022-11-03 15:40:13 +0600

    Feature Updates! * Automatic Birthday Wisher Working Fine!

--- Automatic Birthday Wisher/main.py
+++ Automatic Birthday Wisher/main.py
@@ -1,5 +1,10 @@
+import smtplib
+
 import pandas as pd
+import random
 from datetime import datetime
+userEmail = "mamunkhan3523@gmail.com"
+userPass = "aefgfgzizrctgrna"
 today = datetime.now()
 todayDate = (today.month, today.day)
 
@@ -7,9 +12,20 @@ birthday = pd.read_csv("./Birthday Data/birthdays.csv")
 birthdayDict = {(row["month"], row["day"]): row for (index, row) in birthday.iterrows()}
 
 if todayDate in birthdayDict:
-    print(f"Your Name : {birthdayDict['row']}")
-
-
+    birthPerson = birthdayDict[todayDate]
+    file_path = f"./letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file=file_path, mode='r') as file:
+        newText = file.read()
+        newText = newText.replace("[NAME],", birthPerson["name"])
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=userEmail, password=userPass)
+        connection.sendmail(from_addr=userEmail, to_addrs="mamunkhan3523@yahoo.com", msg=f"Subject: Happy Birthday "
+                                                                                         f"{birthPerson['name']}\n\n"
+                                                                                         f"{newText}")
+        print("Email Sent Successfully!")
+else:
+    print("No Birthday Today")
 # birthdayMonth = str(birthdays)
 # print(birthdayMonth)
 # print(type(birthdayMonth))
