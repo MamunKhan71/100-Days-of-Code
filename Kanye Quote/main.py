0bb387d29238408089c851be3bc807fa56883a04
Md. Mamun <mkmamun031@gmail.com>
2022-11-04 20:46:10 +0600

    #Feature Updates** ** Working with API ** Kayne Quote Maker Software working fine.

--- Kanye Quote/main.py
+++ Kanye Quote/main.py
@@ -1,16 +1,27 @@
 from tkinter import *
-import response
+import requests
+
+
+def quoteChanger():
+    serverResponse = requests.get(url="https://api.kanye.rest/").json()
+    newQuote = serverResponse["quote"]
+    canvas.itemconfig(canvasText, text=newQuote)
+
 
 window = Tk()
+
+serverResponse = requests.get(url="https://api.kanye.rest/").json()
 window.title("Kanye Says")
 window.config(padx=20, pady=20)
-canvas = Canvas(height=400, width=500, highlightthickness=0)
-image = PhotoImage(file="quoteBackground.png")
+canvas = Canvas(height=414, width=300, highlightthickness=0)
+# image = PhotoImage(file="quoteBackground.png")
 canvas.config(bg="white")
-canvas.create_image(250, 220, image=image)
+# canvas.create_image(250, 220, image=image)
 canvas.grid(row=0, column=0, columnspan=1)
+canvasText = canvas.create_text(150, 207, text="Hello there", font=("Consolas", 10, "bold"),width=250)
+quoteChanger()
 face = PhotoImage(file="icon.png")
-button = Button(image=face, highlightthickness=0, bg="white", width=500)
-button.grid(row=1,column=0, columnspan=1)
+button = Button(image=face, highlightthickness=0, bg="white", width=300, command=quoteChanger)
+button.grid(row=1, column=0, columnspan=1)
 
 window.mainloop()
