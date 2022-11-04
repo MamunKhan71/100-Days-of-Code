8ce816a9b50c337debad5edd838896727dabf877
Md. Mamun <mkmamun031@gmail.com>
2022-11-04 23:20:18 +0600

    Kanye Quote App Updates

---
+++ Practice/apiPractice.py
@@ -0,0 +1,20 @@
+import requests
+from datetime import datetime
+myLat = 23.900372
+myLng = 90.327225
+formatting = 0
+parameter = {
+    "lat": myLat,
+    "lng": myLng,
+    "formatted": formatting,
+}
+now = datetime.now()
+sunStatus = requests.get(url="https://api.sunrise-sunset.org/json", params=parameter)
+sunStatus.raise_for_status()
+data = sunStatus.json()
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+print(sunrise)
+print(sunset)
+print(now)
+
