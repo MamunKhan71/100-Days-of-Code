5060b7ed24c790688399400803dcb22b5c72d750
Md. Mamun <mkmamun031@gmail.com>
2022-11-04 13:10:39 +0600

    Kanye Quote App Updates * Added Background Image for code * Code Updates

--- Kanye Quote/main.py
+++ Kanye Quote/main.py
@@ -1,13 +1,16 @@
 from tkinter import *
+import response
 
 window = Tk()
 window.title("Kanye Says")
 window.config(padx=20, pady=20)
 canvas = Canvas(height=400, width=500, highlightthickness=0)
 image = PhotoImage(file="quoteBackground.png")
-canvas.config(bg="#55a630")
-canvas.create_image(250, 200, image=image)
-canvas.grid(row=0, column=1, columnspan=1)
-
+canvas.config(bg="white")
+canvas.create_image(250, 220, image=image)
+canvas.grid(row=0, column=0, columnspan=1)
+face = PhotoImage(file="icon.png")
+button = Button(image=face, highlightthickness=0, bg="white", width=500)
+button.grid(row=1,column=0, columnspan=1)
 
 window.mainloop()
