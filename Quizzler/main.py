36025e7a9d5208ce71ed144138a499ff7928fa42
Md. Mamun <mkmamun031@gmail.com>
2022-11-06 01:20:51 +0600

    # Feature Updates (Quizzler) -> UI Updates -> Several Code Fixes

--- Quizzler/main.py
+++ Quizzler/main.py
@@ -2,16 +2,18 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizBrain
 from tkinter import *
+import html
 window = Tk()
 window.title("Quizzler - The Quiz App")
-window.config(padx=50,pady=50, bg="#ededed", highlightthickness=0)
+window.config(padx=50, pady=50, bg="#ededed", highlightthickness=0)
 canvas = Canvas(height=400, width=400, highlightthickness=0, bg="#ededed")
 score = Label(text="Score", font=("Arial", 20, "bold"), bg="#ededed")
 score.grid(row=0,column=1, columnspan=2)
+
 bgImage = PhotoImage(file="./images/bg1.png")
 canvas.create_image(200,200, image=bgImage)
 canvas.grid(row=1, column=1, columnspan=2)
-question = canvas.create_text(200,200, text="Hello there, My name is Md. Mamun. I am from Dhaka", width=200, font=("Arial", 20, "bold"), fill="white", justify=CENTER)
+question2 = canvas.create_text(200,200, text="Hello there, My name is Md. Mamun. I am from Dhaka", width=200, font=("Calibiri", 20, "bold"), fill="white", justify=CENTER)
 rImage = PhotoImage(file="./images/true.png")
 rightButton = Button(image=rImage, borderwidth=0)
 rightButton.grid(row=2,column=1)
@@ -19,20 +21,22 @@ lImage = PhotoImage(file="./images/false.png")
 
 leftButton = Button(image=lImage, borderwidth=0)
 leftButton.grid(row=2, column=2)
-window.mainloop()
 
-# question_bank = []
-# for question in question_data:
-#     question_text = question["question"]
-#     question_answer = question["correct_answer"]
-#     new_question = Question(question_text, question_answer)
-#     question_bank.append(new_question)
-#
-#
-# quiz = QuizBrain(question_bank)
-#
-# while quiz.still_has_questions():
-#     quiz.next_question()
-#
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    nextQ = quiz.next_question()
+    canvas.itemconfig(question2, text=html.unescape(nextQ))
+
+
 # print("You've completed the quiz")
 # print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+window.mainloop()
--- Quizzler/quiz_brain.py
+++ Quizzler/quiz_brain.py
@@ -5,6 +5,7 @@ class QuizBrain:
         self.score = 0
         self.question_list = q_list
         self.current_question = None
+    # def quizWriter(self):
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
@@ -12,16 +13,18 @@ class QuizBrain:
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {self.current_question.text} (True/False): ")
-        self.check_answer(user_answer)
+        return f"Q.{self.question_number}: {self.current_question.text}"
 
-    def check_answer(self, user_answer):
-        correct_answer = self.current_question.answer
-        if user_answer.lower() == correct_answer.lower():
-            self.score += 1
-            print("You got it right!")
-        else:
-            print("That's wrong.")
 
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
+
+
+    # def check_answer(self, user_answer):
+    #     correct_answer = self.current_question.answer
+    #     if user_answer.lower() == correct_answer.lower():
+    #         self.score += 1
+    #         # print("You got it right!")
+    #     else:
+    #         # print("That's wrong.")
+    #
+    #     # print(f"Your current score is: {self.score}/{self.question_number}")
+    #     # print("\n")
