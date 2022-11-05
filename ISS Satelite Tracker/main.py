e4e057d887246e39997ff2f33ad792ad29209f0f
Md. Mamun <mkmamun031@gmail.com>
2022-11-05 12:30:39 +0600

    ISS Overhead Working * Logic optimizations * Iss tracker now works by validation the condition whether it is night or day. [ If it's night, then, the ISS can be seen, else can't be seen ]

--- ISS Satelite Tracker/main.py
+++ ISS Satelite Tracker/main.py
@@ -1,14 +1,29 @@
 import requests
+from datetime import datetime
 
 my_lat = 23.810331
 my_lng = 90.412521
+time = datetime.now().hour
+
+parameters = {
+    "lat": my_lat,
+    "lng": my_lng,
+}
 
 
 def issIsOverhead(iss_lt, iss_lg):
-    if (iss_lg - 5) <= my_lng <= (iss_lg + 5) and (iss_lt - 5) <= my_lat <= (iss_lt + 5) :
-        print("True")
+    if (iss_lg - 5) <= my_lng <= (iss_lg + 5) and (iss_lt - 5) <= my_lat <= (iss_lt + 5):
+        if isNightCheck():
+            print("Can be seen")
+        else:
+            print("Can't be seen")
     else:
         print("False")
+
+
+def isNightCheck():
+    if time > sunset or time < sunrise:
+        return True
 
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
@@ -18,3 +33,9 @@ iss_lat = float(issData["iss_position"]["latitude"])
 
 iss_lng = float(issData["iss_position"]["longitude"])
 issIsOverhead(iss_lat, iss_lng)
+
+rspns = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+rspns.raise_for_status()
+timeData = rspns.json()
+sunset = timeData["results"]["sunset"]
+sunrise = timeData["results"]["sunrise"]
