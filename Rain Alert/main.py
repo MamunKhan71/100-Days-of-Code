f6ea119c5019eb1c3a0cb9ee857e2f90decb46e6
Md. Mamun <mkmamun031@gmail.com>
2022-11-08 01:36:53 +0600

    # Feature Updates (QuizzLet) -> Added rain detection functionality. -> More will be added later!

--- Rain Alert/main.py
+++ Rain Alert/main.py
@@ -6,9 +6,19 @@ parameter = {
     "lat": 23.900372,
     "lon": 90.327225,
     "appid": apiKey,
+    "exclude": "minutely,current,daily",
 }
 
 fetched = requests.get(url="https://api.openweathermap.org/data/2.5/onecall?", params=parameter)
 fetched.raise_for_status()
-fetchedData = fetched.json()
-print(fetchedData)
+fetchedData = fetched.json()["hourly"][:12]
+umbrellaBringer = False
+for num in fetchedData:
+    if fetchedData[0]["weather"][0]["id"] < 700:
+        umbrellaBringer = True
+    else:
+        continue
+if umbrellaBringer:
+    print("Bring an umbrella")
+else:
+    print("Umbrella not needed")
