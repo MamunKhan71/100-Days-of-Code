d4c391774f0a92661195a46d3765f9858013d31d
Md. Mamun <mkmamun031@gmail.com>
2022-11-07 13:51:01 +0600

    # Feature Updates (QuizzLet) -> UI Updates -> Several Code Fixes

--- Quizzler/ui.py
+++ Quizzler/ui.py
@@ -14,7 +14,7 @@ class QuizInterface:
         self.score = Label(text="Score", font=("Arial", 20, "bold"), bg="#ededed")
         self.score.grid(row=0, column=1, columnspan=2)
         self.bgImage = PhotoImage(file="images/bg1.png")
-        self.canvas.create_image(200, 200, image=self.bgImage)
+        self.image = self.canvas.create_image(200, 200, image=self.bgImage)
         self.canvas.grid(row=1, column=1, columnspan=2)
         self.questionText = self.canvas.create_text(200, 200, text="Question Text",
                                                     width=200,
@@ -43,8 +43,10 @@ class QuizInterface:
 
     def feedback(self, checkAnswer):
         if checkAnswer:
-            self.canvas.config(bg="green")
+            newImage = PhotoImage(file="./images/bgGreen.png")
+            self.canvas.itemconfig(self.image, image=newImage)
             self.window.after(1000, func=self.get_next_qus)
         else:
-            self.canvas.config("red")
-            self.window.after(1000,func=self.get_next_qus)
+            newImage = PhotoImage(file="./images/bgRed.png")
+            self.canvas.itemconfig(self.image, image=newImage)
+            self.window.after(1000, func=self.get_next_qus)
