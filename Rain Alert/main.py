507841011540ba958f8ad6f4fb8dacee9a6f2f25
Md. Mamun <mkmamun031@gmail.com>
2022-11-09 16:36:47 +0600

    # Feature Updates (QuizzLet) -> Added rain detection functionality. -> More will be added later!

--- Rain Alert/main.py
+++ Rain Alert/main.py
@@ -1,10 +1,12 @@
 import requests
-
+from twilio.rest import Client
+account_sid = "AC2376c41ec068151bf6a1db34ac98028c"
+auth_token = "429ab6e089d790ba0592d3bbfd1ab771"
 apiKey = "69f04e4613056b159c2761a9d9e664d2"
 
 parameter = {
-    "lat": 23.900372,
-    "lon": 90.327225,
+    "lat": 11.237490,
+    "lon": 124.989166,
     "appid": apiKey,
     "exclude": "minutely,current,daily",
 }
@@ -19,6 +21,12 @@ for num in fetchedData:
     else:
         continue
 if umbrellaBringer:
-    print("Bring an umbrella")
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="Oh NO!! Seems Like It will Rain Today - Bring an Umbrella",
+        from_="+18658003523",
+        to="+12139156465",
+    )
+    print(message.status)
 else:
     print("Umbrella not needed")
