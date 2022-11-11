0b43caadade1706676bcd113828c1cdedb30f02c
Md. Mamun <mkmamun031@gmail.com>
2022-11-11 14:00:47 +0600

    # Workout Tracker Application -> Workout Tracker is now working fine. -> Fixed bug that was causing problem to connect with the api.

--- Workout Tracker/main.py
+++ Workout Tracker/main.py
@@ -1,5 +1,6 @@
 import requests
 from datetime import datetime
+
 applicantID = "724fd8aa"
 gender = "Male"
 weight = 58
@@ -30,9 +31,27 @@ exParameter = {
 }
 
 response = requests.post(url=exerciseEndPoint, json=exParameter, headers=headers)
+data = response.json()
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+for dta in data["exercises"]:
+    sheetyParameter = {
+        "workout": {
+                "date": today_date,
+                "time": now_time,
+                "exercise": dta["name"].title(),
+                "duration": dta["duration_min"],
+                "calories": dta["nf_calories"],
+            }
+        }
+    post = requests.post(url=sheetyApiEndPoint, json=sheetyParameter)
+    post.raise_for_status()
+    print(post.text)
+    # sheetyParameter = {
+    #     "date": date.strftime("%d/%m/%Y"),
+    #     "time": date.strftime("%H%M%S"),
+    #     "exercise": f"(response.json()['exercises'][dt]['name'])",
+    #     "duration": f"(response.json()['exercises'][dt]['duration_min'])",
+    #     "calories": f"(response.json()['exercises'][dt]['nf_calories'])",
+    # }
 
-sheetyParameter = {
-    "Date": date.strftime("%d/%m/%Y"),
-    "Time": date.strftime("%H%M%S"),
-    "Exercise":
-}
