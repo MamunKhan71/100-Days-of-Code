3324f965f301d725504ca2d6172a716c73000e48
Md. Mamun <mkmamun031@gmail.com>
2022-11-11 14:44:06 +0600

    # Workout Tracker Application -> Added User Authentication system before accessing the Google sheet to prevent unauthorized access.

--- Workout Tracker/main.py
+++ Workout Tracker/main.py
@@ -1,21 +1,23 @@
 import requests
 from datetime import datetime
-
+import os
 applicantID = "724fd8aa"
 gender = "Male"
 weight = 58
 height = 167
 age = 23
 date = datetime.now()
-appKey = "0715a4a127c10cbff7bbf4477398b227"
-shettyAuth = "Basic bWFtdW4wMzE6bWFtdW5raGFuMDMx"
+SHETTY_AUTH = os.environ.get('SHETTY_AUTH')
+print(SHETTY_AUTH)
+API_KEY = os.environ.get("API_KEY")
+
 sheetyApiEndPoint = "https://api.sheety.co/f3f2d6c9e76f4500056fda7540ff6199/myWorkouts/workouts"
 apiEndPoint = "https://trackapi.nutritionix.com/v2/natural/nutrients"
 exerciseEndPoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 userInput = input("Which exercise you did: ")
 headers = {
     "x-app-id": applicantID,
-    "x-app-key": appKey,
+    "x-app-key": os.environ.get("API_KEY"),
     "x-remote-user-id": "0",
 }
 parameter = {
@@ -31,24 +33,25 @@ exParameter = {
 
 }
 sheetyHeader = {
-    "Authorization": shettyAuth,
+    "Authorization": os.environ.get("SHETTY_AUTH"),
 }
 response = requests.post(url=exerciseEndPoint, json=exParameter, headers=headers)
 data = response.json()
-today_date = datetime.now().strftime("%d/%m/%Y")
-now_time = datetime.now().strftime("%X")
-for dta in data["exercises"]:
-    sheetyParameter = {
-        "workout": {
-                "date": today_date,
-                "time": now_time,
-                "exercise": dta["name"].title(),
-                "duration": dta["duration_min"],
-                "calories": dta["nf_calories"],
-            }
-        }
-    post = requests.post(url=sheetyApiEndPoint, json=sheetyParameter, headers=sheetyHeader)
-    post.raise_for_status()
-    print(post.text)
-
-
+print(data)
+# today_date = datetime.now().strftime("%d/%m/%Y")
+# now_time = datetime.now().strftime("%X")
+# for dta in data["exercises"]:
+#     sheetyParameter = {
+#         "workout": {
+#                 "date": today_date,
+#                 "time": now_time,
+#                 "exercise": dta["name"].title(),
+#                 "duration": dta["duration_min"],
+#                 "calories": dta["nf_calories"],
+#             }
+#         }
+#     post = requests.post(url=sheetyApiEndPoint, json=sheetyParameter, headers=sheetyHeader)
+#     post.raise_for_status()
+#     print(post.text)
+#
+#
