3c912442a8f24137bbbcce5980d78ac6db315d05
Md. Mamun <mkmamun031@gmail.com>
2022-11-12 14:06:30 +0600

    # The Cheapest Flight Tracker -> Added Flight Data

--- Flight Tracker/flight_data.py
+++ Flight Tracker/flight_data.py
@@ -1,7 +1,14 @@
-
 import requests
-flightSearch = "https://api.tequila.kiwi.com/v2/search"
-class FlightData:
-    #This class is responsible for structuring the flight data.
-    pass
 
+flightSearch = "https://api.tequila.kiwi.com/v2/search"
+
+
+class FlightData:
+    def __init__(self, price, org_city, org_airport, dest_city, dest_airport, out_date, return_date):
+        self.price = price
+        self.org_city = org_city
+        self.org_airport = org_airport
+        self.dest_city = dest_airport
+        self.dest_airport = dest_airport
+        self.out_date = out_date
+        self.return_date = return_date
