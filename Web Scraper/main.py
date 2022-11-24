26cd9e4d0a8b9427e672d91e5b67bd6521ef41e6
Md. Mamun <mkmamun031@gmail.com>
2022-11-24 09:32:59 +0600

    Designing the web * Added Web Scrapper!

---
+++ Web Scraper/main.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+response = requests.get("https://news.ycombinator.com/newest")
+webpage = response.text
+article_texts = []
+article_links = []
+article_score = []
+soup = BeautifulSoup(webpage, "html.parser")
+article = soup.find_all(name="span", class_="titleline")
+for articles in article:
+    article_texts.append(articles.getText())
+    article_links.append(articles.get("href"))
+article_score = [int(score.getText().split(" ")[0]) for score in soup.find_all(name="span", class_="score")]
+highest_upvote = int(article_score.index(max(article_score)))
+print(article_texts[highest_upvote])
+
+
+
--- practiceHtmlp/main.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-response = requests.get("https://news.ycombinator.com/")
-webpage = response.text
-
-soup = BeautifulSoup(webpage, "html.parser")
-allLinks = soup.find(name="a", class_="title")
-print(allLinks)
