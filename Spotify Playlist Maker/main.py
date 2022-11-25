6d4d06a091701b927eca744d90d7e95d618b1f7d
Md. Mamun <mkmamun031@gmail.com>
2022-11-25 19:55:21 +0600

    Adding Spotify Playlist Creator * Added playlist checker * Added song searching features * Adding song features!

--- Spotify Playlist Maker/main.py
+++ Spotify Playlist Maker/main.py
@@ -5,6 +5,7 @@ import spotipy
 from pprint import pprint
 from spotipy.oauth2 import SpotifyOAuth
 from datetime import datetime
+play_list_id = None
 client_id = os.getenv("client_id")
 client_secret = os.getenv("client_secret")
 redirectUrl = "http://example.com"
@@ -14,7 +15,7 @@ response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{userDate
 soup = BeautifulSoup(response.text, "html.parser")
 topSong = soup.find_all("div", class_="o-chart-results-list-row-container")
 songList = [song.find('h3').text.strip() for song in topSong]
-
+present = False
 spotifyLogin = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
                                                          client_secret=client_secret, redirect_uri=redirectUrl))
 
@@ -22,18 +23,28 @@ userName = spotifyLogin.me()['display_name']
 userId = spotifyLogin.me()['id']
 print(songList[1])
 songSearch = f"track: {songList[1]} year: {year}"
+playlistCheck = f"{userDate} Billboard 100"
+demoPlayListCheck = 'Music for Videos 📸'
 spotifySearch = spotifyLogin.search(q=songSearch, limit=10, type="track", offset=0)
 
 currentPlaylists = spotifyLogin.current_user_playlists(limit=10)['items']
-for items in currentPlaylists:
-    if items['name'] not in songSearch:
-        playlist = spotifyLogin.user_playlist_create(
-            user=userId,
-            name=f"{userDate} Billboard 100"
-        )
-    else:
-        pass
+
+
+def playlistChecker():
+    global play_list_id
+    for lst in currentPlaylists:
+        if lst['name'] == playlistCheck:
+            play_list_id = lst['id']
+        else:
+            playlist = spotifyLogin.user_playlist_create(
+                        user=userId,
+                        name=f"{userDate} Billboard 100",
+                        public=False,
+                        collaborative=False,
+                        description=f"A playlist of top 100 from {userDate}"
+                    )
+    pass
+
 
 pprint(currentPlaylists[0]['name'])
-
-
+pprint(currentPlaylists)
