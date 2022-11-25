1f5e7a672080cb57bcdb77829468d66e59fbba6b
Md. Mamun <mkmamun031@gmail.com>
2022-11-25 11:42:03 +0600

    Adding Spotify Playlist Creator * Fetching song data from the website is now working. * Optimized the code with List Comprehension

--- Spotify Playlist Maker/main.py
+++ Spotify Playlist Maker/main.py
@@ -3,11 +3,7 @@ from bs4 import BeautifulSoup
 
 userDate = input("Type the date you want (YYYY-MM-DD) : ")
 response = requests.get(url=f"https://www.billboard.com/charts/hot-100/{userDate}")
-
 soup = BeautifulSoup(response.text, "html.parser")
-topSong = soup.find("h3", id="title-of-a-story",
-                    class_="c-title  a-no-trucate a-font-primary-bold-s u-letter-spacing-0021 "
-                           "lrv-u-font-size-18@tablet lrv-u-font-size-16 u-line-height-125 "
-                           "u-line-height-normal@mobile-max a-truncate-ellipsis "
-                           "u-max-width-330 u-max-width-230@tablet-only")
-print(topSong)
+topSong = soup.find_all("div", class_="o-chart-results-list-row-container")
+songList = [song.find('h3').text.strip() for song in topSong]
+print(songList)
