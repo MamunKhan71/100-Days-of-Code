134fa75467feeeea0d2f0041c8b6543ef5cd8793
Md. Mamun <mkmamun031@gmail.com>
2022-11-27 01:59:13 +0600

    Added Amazon Price Tracker -> Fixed Several Bugs!

---
+++ Practice/practc.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+chrome_driver_app = f"C:\chromedriver"`
+driver = Service(chrome_driver_app)
+
+driver = webdriver.Chrome(chrome_driver_app)
+driver.get(url="https://www.amazon.com/Instant-Pot-Ultra-Programmable-Sterilizer/dp/B07588SJHN/ref=ex_alt_wg_d"
+               "?_encoding=UTF8&pd_rd_i=B07588SJHN&psc=1&pd_rd_w=t7Ud0&pf_rd_p=4e1b46a8-daf9-4433-b97e-d6df97cf3699"
+               "&pf_rd_r=8EY0YVPAP31Y8RS5MK9N&pd_rd_wg=ptXRO&pd_rd_r=312ff4fa-7c61-4ac3-ba95-8d8028b11a3e&content-id"
+               "=amzn1.sym.4e1b46a8-daf9-4433-b97e-d6df97cf3699")
+price = driver.find_element("By.ID", "a-autoid-11-announce")
+print(price)
+driver.quit()
+
