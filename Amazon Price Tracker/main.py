8774ea27cd9f93d9b7ef88a0db7da7735d99adb0
Md. Mamun <mkmamun031@gmail.com>
2022-11-26 23:35:41 +0600

    Added Amazon Price Tracker -> All the functionality is now working fine.

--- Amazon Price Tracker/main.py
+++ Amazon Price Tracker/main.py
@@ -1,7 +1,9 @@
 import requests
-from pprint import pprint
+import smtplib
 from bs4 import BeautifulSoup
-
+import os
+gmail_email = os.getenv("gmail_email")
+gmail_passWord = os.getenv("gmail_passWord")
 url = "https://www.amazon.com/Instant-Pot-Ultra-Programmable-Sterilizer/dp/B07588SJHN/ref=ex_alt_wg_d?_encoding=UTF8&pd_rd_i=B07588SJHN&pd_rd_w=t7Ud0&pf_rd_p=4e1b46a8-daf9-4433-b97e-d6df97cf3699&pf_rd_r=8EY0YVPAP31Y8RS5MK9N&pd_rd_wg=ptXRO&pd_rd_r=312ff4fa-7c61-4ac3-ba95-8d8028b11a3e&content-id=amzn1.sym.4e1b46a8-daf9-4433-b97e-d6df97cf3699&th=1"
 
 headers = {
@@ -15,5 +17,16 @@ soup = BeautifulSoup(response.text, features="lxml")
 price = soup.find("span", class_="a-price-whole")
 priceFraction = soup.find("span", class_="a-price-fraction")
 priceConvert = float(f"{price.getText()}{priceFraction.getText()}")
+if priceConvert < 90:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=userEmail, password=passWord)
+        connection.sendmail(from_addr=userEmail,
+                            to_addrs="mkmamun031@gmail.com",
+                            msg=f"Subject:Buy Now \n\n Price is under 90$. Buy Now or "
+                                "loose at your own risk!")
+else:
+    print("Price is not under 90$")
+
 
 
