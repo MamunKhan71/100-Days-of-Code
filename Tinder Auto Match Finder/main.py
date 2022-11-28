b045faf551e45e40346efff246c6df1bb6ed2835
Md. Mamun <mkmamun031@gmail.com>
2022-11-28 15:10:59 +0600

    Adding LinkedIn Job Application Bot -> Automatic Log in now working fine!

---
+++ Tinder Auto Match Finder/main.py
@@ -0,0 +1,27 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+chrome = "C:\chromedriver"
+driver = webdriver.Chrome(chrome)
+driver.get(url="https://tinder.com/")
+
+time.sleep(2)
+login = driver.find_element(By.XPATH, '//*[@id="q798806120"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a/div[2]/div[2]')
+login.click()
+time.sleep(1)
+fb_login = driver.find_element(By.XPATH, '//*[@id="q-929574956"]/main/div/div[1]/div/div/div[3]/span/div[2]/button/div[2]/div[2]')
+time.sleep(1)
+fb_login.click()
+fb_window = driver.window_handles[1]
+driver.switch_to.window(fb_window)
+time.sleep(2)
+fb_email = driver.find_element(By.NAME, "email")
+fb_email.send_keys("01643091606")
+fb_pass = driver.find_element(By.NAME, "pass")
+fb_pass.send_keys("AimDaP,IwCtpuD.Ia24n")
+login_btn = driver.find_element(By.NAME, "login")
+login_btn.click()
+time.sleep(10)
+
+
