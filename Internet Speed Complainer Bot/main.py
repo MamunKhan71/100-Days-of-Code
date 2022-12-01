fae0cfa3dfd0ad65268b2ee394cb1cf036140cb7
Md. Mamun <mkmamun031@gmail.com>
2022-12-01 22:48:00 +0600

    Adding Twitter Auto Internet Speed Complainer Bot!

--- Internet Speed Complainer Bot/main.py
+++ Internet Speed Complainer Bot/main.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import os
@@ -9,3 +11,11 @@ twitter_pass = os.getenv("twitter_pass")
 
 driver = webdriver.Chrome(chrome_driver)
 driver.get(url="https://twitter.com/")
+login = driver.find_element(By.XPATH, '//*[@id="layers"]/div/div[1]/div/div/div/div/div[2]/div/div/div[1]/a/div')
+login.click()
+email = driver.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input')
+email.send_keys(twitter_id)
+time.sleep(2)
+passWord = driver.find_element(By.XPATH, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')
+passWord.send_keys(twitter_pass)
+time.sleep(5)
