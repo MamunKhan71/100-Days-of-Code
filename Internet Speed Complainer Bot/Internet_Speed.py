aaf966227b88cd3c5c262b53714a61ef2bb71e28
Md. Mamun <mkmamun031@gmail.com>
2022-12-02 00:47:43 +0600

    Adding Twitter Auto Internet Speed Complainer Bot! -> Automatic Login Now Working Fine! -> Adding Another Class to test the current internet speed!

--- Internet Speed Complainer Bot/Internet_Speed.py
+++ Internet Speed Complainer Bot/Internet_Speed.py
@@ -1,5 +1,8 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
 
 class InternetSpeedMeter:
     def __init__(self):
@@ -7,5 +10,14 @@ class InternetSpeedMeter:
         self.download_speed = 0
         self.website_name = "https://www.speedtest.net/"
         self.chrome_driver = "C:\chromedriver"
+
     def get_upload_speed(self):
+        driver = webdriver.Chrome(self.chrome_driver)
+        driver.get(url=self.website_name)
+        time.sleep(5)
+        get_speed = driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]')
+        get_speed.submit()
+        # self.download_speed = driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div['
+        #                                                     '3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div['
+        #                                                     '1]/div/div[2]/span')
 
