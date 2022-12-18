7e26ccd977865d30fd7f18e870dc747c2abeb568
= <=>
2022-12-18 10:23:01 +0600

    Creating Web By using python decorator function!

--- Flask/hello.py
+++ Flask/hello.py
@@ -3,12 +3,20 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def bold_maker(function):
+    def wrapper_f(*args):
+        return f"<b>{args[0]}</b>"
+
+    return wrapper_f
+
+
 @app.route("/")
 def say_hi():
     return "Hello Hi!"
 
 
-@app.route("/<users>")
+@app.route("/bye")
+@bold_maker
 def greet(users):
     return f"Hello Hi {users}"
 
@@ -16,3 +24,5 @@ def greet(users):
 if __name__ == "__main__":
     app.run(debug=True)
     say_hi()
+
+greet("Mamun")
