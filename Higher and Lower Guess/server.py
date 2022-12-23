36e3cb6f83a58cfb63a976bdaa4b9c8212f32a54
= <=>
2022-12-23 23:44:52 +0600

    Higher and lower is now working fine! -> Game Completed!

--- Higher and Lower Guess/server.py
+++ Higher and Lower Guess/server.py
@@ -6,24 +6,6 @@ randomNum = random.randint(0, 9)
 userInput = 0
 
 
-def dec(fn):
-    def wrapper():
-        if userInput < randomNum:
-            return "<h1 align='center'>Too Low, Try Again!!</h1> <br>" \
-                   "<center><img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif' width=300px " \
-                   "</center></img> "
-        elif userInput > randomNum:
-            return "<h1 align='center'>Too High, Try Again!!</h1> <br>" \
-                   "<center><img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif' width=300px " \
-                   "</center></img> "
-        else:
-            return "<h1 align='center'>You Found ME!</h1> <br>" \
-                   "<center><img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif' width=300px " \
-                   "</center></img> "
-
-    return wrapper
-
-
 @app.route('/')
 def sayhi():
     return "<h1 align='center'>Guess the number between 0 and 9</h1> <br>" \
@@ -32,8 +14,20 @@ def sayhi():
 
 @app.route('/<int:number>')
 def inputGetter(number):
-    global userInput
+    global userInput, randomNum
     userInput = number
+    if userInput < randomNum:
+        return "<h1 align='center'>Too Low, Try Again!!</h1> <br>" \
+               "<center><img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif' width=300px " \
+               "</center></img> "
+    elif userInput > randomNum:
+        return "<h1 align='center'>Too High, Try Again!!</h1> <br>" \
+               "<center><img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif' width=300px " \
+               "</center></img> "
+    else:
+        return "<h1 align='center'>You Found ME!</h1> <br>" \
+               "<center><img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif' width=300px " \
+               "</center></img> "
 
 
 if __name__ == "__main__":
