cd92941122da2f290eb5f679a4952092ae3e4ad4
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2023-01-04 14:53:31 +0600

    Fixed #4
    Page post not working issue fixed!

--- News Blog V2/main.py
+++ News Blog V2/main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 import requests
 app = Flask(__name__)
-
+url = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391").json()
+total_news = len(url)
 @app.route('/')
 def sayhi():
-    return render_template('index.html', urls=url)
+    return render_template('index.html', urls=url, total_news=total_news)
 
 @app.route("/home")
 def home():
@@ -18,8 +19,11 @@ def about():
 def contact():
     return render_template("contact.html")
 
+@app.route("/post/<pid>")
+def post(pid):
+    return render_template("post.html", news=url, pd=int(pid))
 # Request Section
-url = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391").json()
+
 
 
 
