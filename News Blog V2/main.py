b5d90e9c43fe1666c2a9adb55613f87bcfd97594
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2023-01-05 00:10:38 +0600

    Fixed Sending Email #7
    Sending Email is now working just fine.

--- News Blog V2/main.py
+++ News Blog V2/main.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request
+import smtplib
+import os
+user_email = os.getenv("userEmail")
+user_pass = os.getenv("userPass")
 import requests
 app = Flask(__name__)
 url = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391").json()
@@ -22,8 +26,12 @@ def post(pid):
 @app.route("/contact", methods=["GET","POST"])
 def contact():
    if request.method == "POST":
-       print(
-           f"Name: {request.form['name']}\nEmail:{request.form['email']}\nPhone: {request.form['phone']}\nMessage: {request.form['message']}")
+       mess = f"Subject: Email From {request.form['name']}\n\nName: {request.form['name']}\nEmail:{request.form['email']}\nPhone: {request.form['phone']}\nMessage: {request.form['message']}"
+       with smtplib.SMTP("smtp.gmail.com") as connection:
+           connection.starttls()
+           connection.login(user=user_email, password=user_pass)
+           connection.sendmail(from_addr=request.form["email"], to_addrs=user_email, msg=mess)
+           print("SMS Sending Successful")
        return render_template("contact.html", sms_sent=True)
    else:
        return render_template("contact.html", sms_sent=False)
