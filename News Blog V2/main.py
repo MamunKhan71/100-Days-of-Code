d88334ebe4010ee120b53e9f3250dc5a319758d5
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2023-01-04 23:25:40 +0600

    Fixed The Contact Form #6
    Fixed getting the input from the contact form. Now it's getting the user input via HTML get and post method!

--- News Blog V2/main.py
+++ News Blog V2/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 app = Flask(__name__)
 url = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391").json()
@@ -15,30 +15,18 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
 @app.route("/post/<pid>")
 def post(pid):
     return render_template("post.html", news=url, pd=int(pid))
-# Request Section
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@app.route("/contact", methods=["GET","POST"])
+def contact():
+   if request.method == "POST":
+       print(
+           f"Name: {request.form['name']}\nEmail:{request.form['email']}\nPhone: {request.form['phone']}\nMessage: {request.form['message']}")
+       return "<h1> Message Sent Successful! </h1>"
+   else:
+       return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
