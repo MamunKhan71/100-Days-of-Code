1470c243d27c7bdf185bfab3e1c7e6e5b230d8e5
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2023-01-06 00:19:17 +0600

    Working with Flask-Bootstrap!

--- secrets/main.py
+++ secrets/main.py
@@ -3,8 +3,9 @@ from flask import Flask, render_template
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 from flask_wtf import FlaskForm
-
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+Bootstrap(app)
 app.secret_key = "any-string-you-want-just-keep-it-secret"
 
 
