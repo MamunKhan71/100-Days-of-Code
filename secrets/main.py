5e1f9ed6cfbe4723c5965bfa40716d4a3944deea
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2023-01-05 23:59:21 +0600

    Adapting Template Inherintance for better optimization!

--- secrets/main.py
+++ secrets/main.py
@@ -22,11 +22,12 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
-    login_form.validate_on_submit()
-    if login_form.email.data == "admin@123.com" and login_form.password.data == "admin123":
-        return "<h1>Success!</h1>"
-    else:
-        return "<h1>Failed!</h1>"
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@123.com" and login_form.password.data == "admin123":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+
     return render_template('login.html', form=login_form)
 
 
