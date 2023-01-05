96e07e5955c061d7463dcc16750d07f16e15663c
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2023-01-05 22:55:51 +0600

    HTML form Validator using WTForms
    Using Flask WTForms to generate forms and validate it furthur!

---
+++ secrets/main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+from flask_wtf import FlaskForm
+
+app = Flask(__name__)
+app.secret_key = "any-string-you-want-just-keep-it-secret"
+
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email ', validators=[Email()])
+    password = PasswordField(label='Password ', validators=[Length(min=8)])
+    submit = SubmitField(label="Log In")
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    login_form = LoginForm()
+    login_form.validate_on_submit()
+    if login_form.email.data == "admin@123.com" and login_form.password.data == "admin123":
+        return "<h1>Success!</h1>"
+    else:
+        return "<h1>Failed!</h1>"
+    return render_template('login.html', form=login_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
