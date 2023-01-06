a3efb8902666fa78654c0b11fd0958bcfb94c9da
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2023-01-06 23:23:05 +0600

    Adopted WTF-Bootstrap
    Beautified the form using flask-bootstrap!

--- secrets/main.py
+++ secrets/main.py
@@ -1,23 +1,24 @@
 from flask import Flask, render_template
-
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
-from flask_wtf import FlaskForm
 from flask_bootstrap import Bootstrap
-app = Flask(__name__)
-Bootstrap(app)
-app.secret_key = "any-string-you-want-just-keep-it-secret"
 
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email ', validators=[Email()])
-    password = PasswordField(label='Password ', validators=[Length(min=8)])
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField(label="Log In")
+
+
+app = Flask(__name__)
+app.secret_key = "any-string-you-want-just-keep-it-secret"
+Bootstrap(app)
 
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -28,8 +29,7 @@ def login():
             return render_template("success.html")
         else:
             return render_template("denied.html")
-
-    return render_template('login.html', form=login_form)
+    return render_template("login.html", form=login_form)
 
 
 if __name__ == '__main__':
