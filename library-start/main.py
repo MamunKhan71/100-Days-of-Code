aee59c135ad8774fd86a795747f8979f9109d644
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2023-01-11 13:03:27 +0600

    DataBase Connection Successful
    User can now add new book listings to the database!

--- library-start/main.py
+++ library-start/main.py
@@ -1,57 +1,56 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy, ForeignKey
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from sqlalchemy import create_engine, Integer, Float, String, CHAR, Column
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///D:\library-start\project.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy()
-all_books = []
+Base = declarative_base()
 
 
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    author = db.Column(db.String(250), unique=True, nullable=False)
-    rating = db.Column(db.Float, nullable=False)
+class book_information(Base):
+    __tablename__ = "book_info"
+    book_id = Column("book_id", Integer, primary_key=True)
+    book_name = Column("book_name", String, nullable=False)
+    book_author = Column("book_author", String, nullable=False)
+    book_rating = Column("book_rating", Float, nullable=False)
+
+    def __init__(self, book_id, book_name, book_author, book_rating):
+        self.book_id = book_id
+        self.book_name = book_name
+        self.book_author = book_author
+        self.book_rating = book_rating
 
     def __repr__(self):
-        return f'<Book {self.title}>'
-
-    db.create_all()
+        return f"{self.book_name}"
 
 
-new_book = Book(id=1, title="Harry Porter", author="J.K Rowling", rating=9.3)
-db.session.add(new_book)
-db.session.commit()
-# @app.route('/')
-# def home():
-#     length = len(all_books)
-#     return render_template("index.html", data=all_books, length=length)
-#
-#
-# @app.route("/add", methods=["GET", "POST"])
-# def add():
-#     return render_template('add.html')
-#
-#
-# @app.route('/add_data', methods=["POST"])
-# def received_data():
-#     name = request.form['book_name']
-#     auth = request.form['book_author']
-#     rating = request.form['book_rating']
-#     new_dictionary = {
-#         "title": name,
-#         "author": auth,
-#         "rating": rating,
-#     }
-#     all_books.append(new_dictionary)
-#     return redirect(url_for('success'))
-#
-#
-# @app.route('/success')
-# def success():
-#     return "<h1> Data Added Successfully! </h1>"
-#
-#
+engine = create_engine(url="sqlite:///D:\Starting Files\mydb.db", echo=True)
+Base.metadata.create_all(bind=engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+app = Flask(__name__)
+app.secret_key = "any-string-you-want-just-keep-it-secret"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        bookID = int(request.form['book_id'])
+        bookName = request.form['book_name']
+        bookAuthor = request.form['book_author']
+        bookRating = float(request.form['book_rating'])
+        b1 = book_information(book_id=bookID, book_name=bookName, book_author=bookAuthor, book_rating=bookRating)
+        session.add(b1)
+        session.commit()
+        return "<h1> Data Added Successfully! </h1>"
+    return render_template("add.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
