ad8b3856c8f8ea72fdf1d5cb5913a215dda9323f
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2023-01-12 22:22:41 +0600

    Fixed Issue with Session Controlling
    # Flask Session was creating problem while refreshing the page for second time.
    # Fixed adding book to the database
    # Added a method to check if the book id already exist in the database to avoid unexpected error.

--- library-start/main.py
+++ library-start/main.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
 from sqlalchemy import create_engine, Integer, Float, String, CHAR, Column
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField
+import os
 
 
 class form(FlaskForm):
-    book_name = StringField('Book Name')
+    book_id = IntegerField('Book Name')
     submit = SubmitField("Search")
 
 
@@ -34,38 +35,50 @@ class book_information(Base):
 engine = create_engine(url="sqlite:///D:\Starting Files\mydb.db", echo=True)
 Base.metadata.create_all(bind=engine)
 Session = sessionmaker(bind=engine)
-session = Session()
 
 app = Flask(__name__)
 app.secret_key = "any-string-you-want-just-keep-it-secret"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    session = Session()
+    if os.path.isfile("mydb.db"):
+        all_books = session.query(book_information).all()
+        session.close()
+        return render_template("index.html", book_list=all_books, length=len(all_books))
+    else:
+        return render_template("index.html", book_list=[''], length=0)
 
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
+    session = Session()
     if request.method == "POST":
         bookID = int(request.form['book_id'])
         bookName = request.form['book_name']
         bookAuthor = request.form['book_author']
         bookRating = float(request.form['book_rating'])
-        b1 = book_information(book_id=bookID, book_name=bookName, book_author=bookAuthor, book_rating=bookRating)
-        session.add(b1)
-        session.commit()
-        return "<h1> Data Added Successfully! </h1>"
+        if session.query(book_information).filter_by(book_id=bookID).first() is None:
+            b1 = book_information(book_id=bookID, book_name=bookName, book_author=bookAuthor, book_rating=bookRating)
+            session.add(b1)
+            session.commit()
+            return redirect(url_for('home'))
+        else:
+            session.close()
+            return "book_id already exists in the table!"
     return render_template("add.html")
 
 
 @app.route("/query", methods=["GET", "POST"])
 def query():
+    session = Session()
     newForm = form()
     if request.method == "POST":
-        data = session.query(book_information).filter(book_information.book_name.like(newForm.book_name.data))
-        return f"<hr><h2>Search Result: {data[0].book_author}</h2><hr>"
+        book = session.query(book_information).filter(form.book_id.data)
+        session.delete(book)
+        session.commit()
     return render_template("query.html", form=newForm)
 
 
