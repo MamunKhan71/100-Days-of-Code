a55a193f9a6969e1aef78ba1f6a88dfc46f32757
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2023-01-15 11:35:00 +0600

    Adding a new Capstone Project Name
    Adding a new TOP 10 movie list website!

---
+++ Movie_Project/main.py
@@ -0,0 +1,74 @@
+import pytest as pytest
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from sqlalchemy import create_engine
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
+
+##CREATE DB
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'sqlite:///D:/100days/64 Day 64 - Advanced -My Top 10 Movies Website/Movie_Project/movie_db.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+##CREATE TABLE
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(250), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(250), nullable=True)
+    img_url = db.Column(db.String(250), nullable=False)
+
+    def __init__(self, title, year, description, rating, ranking, review, img_url):
+        self.title = title
+        self.year = year
+        self.description = description
+        self.rating = rating
+        self.ranking = ranking
+        self.review = review
+        self.img_url = img_url
+
+    def __repr__(self):
+        return f"{self.id} {self.title} {self.year} {self.description} {self.rating} {self.ranking} {self.review} {self.img_url}"
+
+
+with app.app_context():
+    db.create_all()
+    new_movie = Movie(
+        title="Phone Booth",
+        year=2002,
+        description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's "
+                    "sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to "
+                    "a jaw-dropping climax.",
+        rating=7.3,
+        ranking=10,
+        review="My favourite character was the caller.",
+        img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+    )
+    existing_movie = Movie.query.filter_by(title=new_movie.title).first()
+    if not existing_movie:
+        db.session.add(new_movie)
+        db.session.commit()
+
+
+@app.route('/')
+def home():
+    movie = db.session.query(Movie).all()
+    return render_template("index.html", movie=movie)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
