540ffce44597aa93c3dddfb986496e55351d3a04
Md. Mamun <mkmamun031@gmail.com>
2023-02-19 23:23:02 +0600

    Code Improvements!

--- MamGPT/main.py
+++ MamGPT/main.py
@@ -4,3 +4,6 @@ chars = sorted(list(set(text)))
 vocab_size = len(chars)
 print(''.join(chars))
 print(vocab_size)
+stoi = {ch: i for i, ch in enumerate(chars)}
+itos = {i: ch for i, ch in enumerate(chars)}
+encode = lambda s: {stoi[c] for c in s}
