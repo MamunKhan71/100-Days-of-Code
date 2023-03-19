1cbab432631385eefe32abad2168e92663ddd2ac
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2023-03-19 20:23:43 +0600

    code improvements!

--- Mini Programming/MorseCode.py
+++ Mini Programming/MorseCode.py
@@ -1,3 +1,80 @@
+def morseDec(letters):
+    match letters:
+        case "/":
+            return " "
+        case ".-":
+            return "A"
+        case "-...":
+            return "B"
+        case "-.-.":
+            return "C"
+        case "-..":
+            return "D"
+        case ".":
+            return "E"
+        case "..-.":
+            return "F"
+        case "--.":
+            return "G"
+        case "....":
+            return "H"
+        case "..":
+            return "I"
+        case ".---":
+            return ".J"
+        case "-.-":
+            return "K"
+        case ".-..":
+            return "L"
+        case "--":
+            return "M"
+        case "-.":
+            return "N"
+        case "---":
+            return "O"
+        case ".--.":
+            return "P"
+        case "--.-":
+            return "Q"
+        case ".-.":
+            return "R"
+        case "...":
+            return "S"
+        case "	-":
+            return "T"
+        case  "..-":
+            return "U"
+        case "...-":
+            return "V"
+        case  ".--":
+            return "W"
+        case "-..-":
+            return "X"
+        case "-.--":
+            return "Y"
+        case "--..":
+            return "Z"
+        case ".----":
+            return "1"
+        case "..---":
+            return "2"
+        case "...--":
+            return "3"
+        case "....-":
+            return "4"
+        case ".....":
+            return "5"
+        case "-....":
+            return "6"
+        case "--...":
+            return "7"
+        case "---..":
+            return "8"
+        case "----.":
+            return "9"
+        case "-----":
+            return "0"
+
 
 def morseEnc(letter):
     match letter:
@@ -84,6 +161,7 @@ print("1. MorseEncode")
 print("2. MorseDecode")
 userData = input(": ")
 userList = []
+newUserList = []
 finalSplit = []
 if userData == "1":
     userText = input("Enter the text you want to Encode: ")
@@ -106,5 +184,11 @@ else:
             finalSplit.append(s)
             if i != len(userText_to_list)-1:
                 finalSplit.append("/")
-
+    print(finalSplit)
+    for letter in finalSplit[:-1]:
+        print(letter)
+        new_list_text = morseDec(letter)
+        newUserList.append(new_list_text)
+    newText = "".join(newUserList)
+    print(f"Your MorseDecoded Text is :\n{newText}")
 
